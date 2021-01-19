0976657d9dfb10ce6f688e8046f70ab2b8186f7d
owefsad <dongzhiyong@secnium.cn>
2021-01-19 10:36:21 +0800

    modify 修改账户模型与业务api一致

--- user/models/talent.py
+++ user/models/talent.py
@@ -5,14 +5,35 @@
 # software: PyCharm
 # project: lingzhi-agent-server
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class AuthTalent(models.Model):
-    talent_name = models.CharField(max_length=255, blank=True, null=True)
+class Talent(models.Model):
+    talent_name = models.CharField(
+        unique=True,
+        verbose_name=_('talent'),
+        max_length=255,
+        blank=True,
+        error_messages={
+            'unique': _("A talent with that talent name already exists."),
+        },
+    )
     create_time = models.IntegerField(blank=True, null=True)
     update_time = models.IntegerField(blank=True, null=True)
     created_by = models.IntegerField(blank=True, null=True)
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
 
     class Meta:
+        verbose_name = _('talent')
         managed = False
         db_table = 'auth_talent'
+
+    def get_talent_name(self):
+        return self.talent_name
