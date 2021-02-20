837b34f6e5f53ebfa033185a1a1af647bd3646f0
owefsad <dongzhiyong@secnium.cn>
2021-02-20 13:03:35 +0800

    modify 修改策略保存接口，支持新增与保存

--- vuln/views/vul_rule_save.py
+++ vuln/views/vul_rule_save.py
@@ -12,7 +12,12 @@ from vuln.models.vul_rule import IastVulRule
 
 
 class VulRuleSaveEndPoint(UserEndPoint):
+    """
+    策略保存API，支持修改、新增
+    """
+
     def post(self, request):
+        rule_id = request.query_params.get('id')
         # todo 数据转换为json保存
         rule_name = request.data.get('id')
         rule_level = request.data.get('level')
@@ -22,16 +27,26 @@ class VulRuleSaveEndPoint(UserEndPoint):
         create_by = request.user.id
         timestamp = int(time.time())
 
-        rule = IastVulRule(
-            rule_name=rule_name,
-            rule_level=rule_level,
-            rule_msg=rule_msg,
-            rule_value=rule_value,
-            is_system=is_system,
-            create_by=create_by,
-            update_time=timestamp,
-            create_time=timestamp
-        )
+        if rule_id:
+            rule = IastVulRule.objects.filter(create_by=request.user.id, id=rule_id).first()
+            rule.rule_value = rule_value
+            rule.rule_name = rule_name
+            rule.rule_level = rule_level
+            rule.rule_msg = rule_msg
+            rule.is_system = is_system
+            rule.create_by = create_by
+            rule.update_time = timestamp
+        else:
+            rule = IastVulRule(
+                rule_name=rule_name,
+                rule_level=rule_level,
+                rule_msg=rule_msg,
+                rule_value=rule_value,
+                is_system=is_system,
+                create_by=create_by,
+                update_time=timestamp,
+                create_time=timestamp
+            )
         rule.save()
 
         return R.success(msg='规则保存成功')
