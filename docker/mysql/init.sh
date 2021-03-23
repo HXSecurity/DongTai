88c183ff6bb059d2e7766bde002f8df8b3dc199c
owefsad <dongzhiyong@secnium.cn>
2021-03-23 20:44:57 +0800

    feature 增加一键打包运行脚本

---
+++ docker/mysql/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "start mysqld"
+nohup /bin/bash /usr/local/bin/docker-entrypoint.sh mysqld &
+
+echo "sleep until msyqld started"
+while :  # 注意加空格
+do
+    ls /var/run/mysqld/mysqld.sock
+    if [ $? -ne 0 ]; then
+	    sleep 1
+    else
+	    break 2
+    fi
+done
+
+sleep 10
+
+echo "init database"
+mysql -uroot -p"dongtai-iast" < /opt/db.sql
+
+echo "hello" > /tmp/a.log
+tail -f /tmp/a.log
---
+++ docker/redis/Dockerfile
@@ -0,0 +1,3 @@
+FROM redis
+COPY redis.conf /usr/local/etc/redis/redis.conf
+CMD [ "redis-server", "/usr/local/etc/redis/redis.conf" ]
