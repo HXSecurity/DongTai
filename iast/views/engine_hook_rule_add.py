a0d1ec79cdcdd3e53966b13560a656292fa739bb
owefsad <dongzhiyong@secnium.cn>
2021-04-01 15:34:44 +0800

    feature 增加sca相关接口Token认证

--- iast/views/engine_hook_rule_add.py
+++ iast/views/engine_hook_rule_add.py
@@ -4,8 +4,8 @@
 # datetime:2021/2/19 下午3:59
 # software: PyCharm
 # project: lingzhi-webapi
-from base.endpoint import SessionAuthProxyView
+from base.endpoint import MixinAuthPoxyView
 
 
-class EngineHookRuleAddEndPoint(SessionAuthProxyView):
+class EngineHookRuleAddEndPoint(MixinAuthPoxyView):
     source = 'api/engine/hook/rule/add'
