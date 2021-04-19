096fb09c375c7998d73bfc98687bdbf45f61a0c9
owefsad <dongzhiyong@secnium.cn>
2021-04-19 15:00:28 +0800

    feature 开放规则类型接口，用于idea插件

--- iast/views/engine_hook_rule_types.py
+++ iast/views/engine_hook_rule_types.py
@@ -4,8 +4,8 @@
 # datetime:2021/2/19 下午3:59
 # software: PyCharm
 # project: lingzhi-webapi
-from base.endpoint import SessionAuthProxyView
+from base.endpoint import MixinAuthPoxyView
 
 
-class EngineHookRuleTypesEndPoint(SessionAuthProxyView):
+class EngineHookRuleTypesEndPoint(MixinAuthPoxyView):
     source = 'api/engine/hook/rule_types'
