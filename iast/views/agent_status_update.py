20d3681ac24ebc7a7a260557f15e3ca49c722e12
owefsad <dongzhiyong@secnium.cn>
2021-04-27 19:24:15 +0800

    feature 增加agent状态更新接口

---
+++ iast/views/agent_status_update.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: owefsad@huoxian.cn
+# datetime: 2021/4/27 下午6:23
+# project: dongtai-webapi
+import time
+
+from django.db.models import Q
+
+from base import R
+from iast.base.agent import AgentEndPoint
+from iast.models.agent import IastAgent
+
+
+class AgentStatusUpdate(AgentEndPoint):
+    def get(self, request):
+        timestamp = int(time.time())
+        queryset = IastAgent.objects.filter(user=request.user)
+        queryset = queryset.filter(
+            (Q(server=None) & (Q(latest_time__lt=(timestamp - 600)))) | Q(server__update_time__lt=(timestamp - 600)),
+            is_running=1)
+        queryset.update(is_running=0)
+        return R.success(msg='引擎状态更新成功')
