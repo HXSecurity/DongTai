7719973bd203294ea72fd70b328e2159a9b15eb9
owefsad <dongzhiyong@secnium.cn>
2021-04-29 21:01:21 +0800

    feature 增加task pod运行定时任务

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,11 @@
 echo '启动uwsgi服务'
 nohup /usr/local/bin/uwsgi --ini /opt/iast/engine/conf/uwsgi.ini &
 sleep 2
-echo '启动celery服务'
-nohup celery -A lingzhi_engine worker -l info -E &
-sleep 5
-celery -A lingzhi_engine beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+
+if [ "$1" == "task"]; then
+  echo '启动celery服务'
+  celery -A lingzhi_engine beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+else
+  echo '启动celery服务'
+  nohup celery -A lingzhi_engine worker -l info -E &
+fi
