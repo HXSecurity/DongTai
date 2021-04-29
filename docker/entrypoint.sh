2f684f976d7dfd9108be42ac0e9d69c5e6249804
owefsad <dongzhiyong@secnium.cn>
2021-04-29 20:44:22 +0800

    feature 增加celery beat启动命令

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,6 @@ echo '启动uwsgi服务'
 nohup /usr/local/bin/uwsgi --ini /opt/iast/engine/conf/uwsgi.ini &
 sleep 2
 echo '启动celery服务'
-celery -A lingzhi_engine worker -l info -E
+nohup celery -A lingzhi_engine worker -l info -E &
+sleep 5
+celery -A lingzhi_engine beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
