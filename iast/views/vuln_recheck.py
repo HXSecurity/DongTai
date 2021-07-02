c0745f78fe9f4ad0627acdfa7f7e04ae011aeb40
owefsad <dongzhiyong@secnium.cn>
2021-07-02 12:24:15 +0800

    feature 增加根据项目批量验证漏洞的接口

--- iast/views/vuln_recheck.py
+++ iast/views/vuln_recheck.py
@@ -7,6 +7,7 @@
 import base64
 import logging
 import time
+from dongtai_models.models.agent import IastAgent
 
 from dongtai_models.models.replay_queue import IastReplayQueue
 from rest_framework.request import Request
@@ -23,6 +24,38 @@ logger = logging.getLogger('dongtai-webapi')
 
 
 class VulReCheck(UserEndPoint):
+    @staticmethod
+    def recheck(vul_queryset):
+        timestamp = int(time.time())
+        waiting_count = 0
+        success_count = 0
+        re_success_count = 0
+        for vul in vul_queryset:
+            history_replay_queryset = IastReplayQueue.objects.filter(relation_id=vul.id,
+                                                                     replay_type=const.VUL_REPLAY).first()
+            if history_replay_queryset:
+                if history_replay_queryset.state in [const.PENDING, const.WAITING, const.SOLVING]:
+                    waiting_count = waiting_count + 1
+                    continue
+                else:
+                    history_replay_queryset.state = const.PENDING
+                    history_replay_queryset.count = history_replay_queryset.count + 1
+                    history_replay_queryset.update_time = timestamp
+                    history_replay_queryset.save(update_fields=['state', 'count', 'update_time', ])
+                    re_success_count = re_success_count + 1
+            else:
+                IastReplayQueue.objects.create(
+                    agent=vul.agent,
+                    relation_id=vul.id,
+                    state=const.PENDING,
+                    count=1,
+                    create_time=timestamp,
+                    update_time=timestamp,
+                    replay_type=const.VUL_REPLAY
+                )
+                success_count = success_count + 1
+        return waiting_count, success_count, re_success_count
+
     def post(self, request):
         """
         :param request:
@@ -33,35 +66,26 @@ class VulReCheck(UserEndPoint):
             vul_ids = vul_ids.split(',')
             auth_agents = self.get_auth_agents_with_user(user=request.user)
             vul_queryset = IastVulnerabilityModel.objects.filter(id__in=vul_ids, agent__in=auth_agents)
-            timestamp = int(time.time())
-            waiting_count = 0
-            success_count = 0
-            re_success_count = 0
-            for vul in vul_queryset:
-                history_replay_queryset = IastReplayQueue.objects.filter(relation_id=vul.id,
-                                                                         replay_type=const.VUL_REPLAY).first()
-                if history_replay_queryset:
-                    if history_replay_queryset.state in [const.PENDING, const.WAITING, const.SOLVING]:
-                        waiting_count = waiting_count + 1
-                        continue
-                    else:
-                        history_replay_queryset.state = const.PENDING
-                        history_replay_queryset.count = history_replay_queryset.count + 1
-                        history_replay_queryset.update_time = timestamp
-                        history_replay_queryset.save(update_fields=['state', 'count', 'update_time', ])
-                        re_success_count = re_success_count + 1
-                else:
-                    IastReplayQueue.objects.create(
-                        agent=vul.agent,
-                        relation_id=vul.id,
-                        state=const.PENDING,
-                        count=1,
-                        create_time=timestamp,
-                        update_time=timestamp,
-                        replay_type=const.VUL_REPLAY
-                    )
-                    success_count = success_count + 1
+            waiting_count, success_count, re_success_count = self.recheck(vul_queryset)
             return R.success(
                 msg=f'{waiting_count}条数据待重放，无需重复验证；{re_success_count}条数据重新下发验证请求；{success_count}条数据已下发验证请求')
         except Exception as e:
             return R.failure(msg=f'漏洞重放出错，错误原因：{e}')
+
+    def get(self, request):
+        # 处理批量重放，例如，项目名称
+        try:
+            project_id = request.query_params.get('projectId')
+            project_exist = IastProject.objects.values("id").filter(id=project_id,
+                                                                    user__in=self.get_auth_users(request.user)).exists()
+            if project_exist:
+                agent_queryset = IastAgent.objects.values("id").filter(bind_project_id=project_id)
+                agent_ids = agent_queryset.values()
+                vul_queryset = IastVulnerabilityModel.objects.filter(agent_id__in=agent_ids)
+                waiting_count, success_count, re_success_count = self.recheck(vul_queryset)
+                return R.success(
+                    msg=f'{waiting_count}条数据待重放，无需重复验证；{re_success_count}条数据重新下发验证请求；{success_count}条数据已下发验证请求')
+            else:
+                return R.failure(msg=f'无权访问项目[{project_id}]')
+        except Exception as e:
+            return R.failure(msg=f'批量重放出错，错误详情：{e}')
