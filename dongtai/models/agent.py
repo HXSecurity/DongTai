98bc1fd4bfd8e79490880b50cc4a3d03e9e03d8e
owefsad <dongzhiyong@secnium.cn>
2021-07-12 20:42:56 +0800

    enhancement 迁移dongtai-model至dongtai.model，合并子项目的const至dongtai项目

---
+++ dongtai/models/agent.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author:owefsad
+# datetime:2020/11/30 下午5:29
+# software: PyCharm
+# project: dongtai-models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from dongtai.models import User
+from dongtai.models.server import IastServerModel
+
+
+class IastAgent(models.Model):
+    token = models.CharField(max_length=255, blank=True, null=True)
+    version = models.CharField(max_length=255, blank=True, null=True)
+    latest_time = models.IntegerField(blank=True, null=True)
+    user = models.ForeignKey(User, models.DO_NOTHING)
+    server = models.ForeignKey(
+        to=IastServerModel,
+        on_delete=models.DO_NOTHING,
+        related_name='agents',
+        related_query_name='agent',
+        verbose_name=_('server'),
+    )
+    is_running = models.IntegerField(blank=True, null=True)
+    is_core_running = models.IntegerField(blank=True, null=True)
+    control = models.IntegerField(blank=True, null=True)
+    is_control = models.IntegerField(blank=True, null=True)
+    bind_project_id = models.IntegerField(blank=True, null=True, default=0)
+    project_name = models.CharField(max_length=255, blank=True, null=True)
+    online = models.PositiveSmallIntegerField(blank=True, default=0)
+    project_version_id = models.IntegerField(blank=True, null=True, default=0)
+
+    class Meta:
+        managed = False
+        db_table = 'iast_agent'
