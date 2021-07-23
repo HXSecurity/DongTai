c72d79673752809454f766bd2074727d76022a7c
bidaya0 <xianzelin984@outlook.com>
2021-07-23 16:03:47 +0800

    项目名称匹配搜索、策略删除和修改

--- iast/views/strategy_modified.py
+++ iast/views/strategy_modified.py
@@ -4,24 +4,36 @@ from dongtai.endpoint import R
 from dongtai.endpoint import UserEndPoint
 from dongtai.models.strategy_user import IastStrategyUser
 from dongtai.models.strategy import IastStrategyModel
+from dongtai.models.hook_type import HookType
 
 
 class StrategyModified(UserEndPoint):
 
-    def put(self, request):
+    def put(self, request, id_):
         '''
         用户修改策略
         '''
-        id_ = request.data.get("id", None)
-        # 策略名称
+        #fields = set(
+        #    get_model_field(HookType, ['id']) +
+        #    get_model_field(IastStrategyModel, exclude=['id']))
+        fields = ['name', 'vul_desc', 'vul_fix', 'enable']
+        data = {k: v for k, v in request.data.items() if k in fields}
+        hook_type = HookType.objects.filter(pk=id_).first()
+        _update(hook_type, data)
         strategy = IastStrategyModel.objects.filter(
-            pk=id_, user_id=request.user.id).first()
-        dic = {}
-        _update(strategy, dic)
+            hook_type=hook_type.id).first()
+        if strategy:
+            _update(strategy, data)
         return R.success(data={"id": id_})
 
 
-def _update(strategy, dic):
+def _update(model, dic):
     for k, v in dic.items():
-        setattr(strategy, k, v)
-    strategy.save()
+        setattr(model, k, v)
+    model.save()
+
+def get_model_field(model, exclude=[],include=[]):
+    fields = [field.name for field in model._meta.fields]
+    if include:
+        return [include for field in list(set(fields) - set(exclude)) if field in include]
+    return list(set(fields) - set(exclude))
