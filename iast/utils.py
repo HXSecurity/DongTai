10af2dd960f3a4af8c456429960a8201630d04eb
bidaya0 <xianzelin984@outlook.com>
2021-07-27 12:30:10 +0800

    add agent delopy document api

---
+++ iast/utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author:Bidaya0
+# datetime:2021/7/27 12:06
+# software: Vim8
+# project: webapi
+
+
+
+def get_model_field(model, exclude=[], include=[]):
+    fields = [field.name for field in model._meta.fields]
+    if include:
+        return [
+            include for field in list(set(fields) - set(exclude))
+            if field in include
+        ]
+    return list(set(fields) - set(exclude))
