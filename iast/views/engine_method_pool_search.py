44c078240f212f1f406e6becfbd1437cb1578d10
bidaya0 <xianzelin984@outlook.com>
2021-07-28 15:03:10 +0800

    add fields relavite in Json in engine_method_pool_search api

--- iast/views/engine_method_pool_search.py
+++ iast/views/engine_method_pool_search.py
@@ -9,7 +9,7 @@ from iast.serializers.method_pool import MethodPoolListSerialize
 from django.db.models import Q
 from django.forms.models import model_to_dict
 from iast.utils import get_model_field
-
+from functools import reduce
 class MethodPoolSearchProxy(AnonymousAndUserEndPoint):
     def get(self, request):
         page_size = request.GET.get('page_size', 10)
@@ -18,22 +18,34 @@ class MethodPoolSearchProxy(AnonymousAndUserEndPoint):
         fields = get_model_field(
             MethodPool,
             include=[
-                'url', 'res_body', 'req_header', 'req_params', 'method_pool'
+                'url', 'res_body', 'req_header', 'req_data'
             ],
         )
+        fields.extend(['sinkvalues', 'signature'])
         searchfields = dict(
             filter(lambda k: k[0] in fields, request.GET.items()))
-        searchfields = {
-            ''.join([k, '__iregex']): v
-            for k, v in searchfields.items()
-        }
-        
-        q = Q()
+        searchfields_ = []
         for k, v in searchfields.items():
-            params = {k: v}
-            q = q | Q(**params)
-#        qset = [Q(**{k: v}) for k, v in searchfields.items()]
-#        q = set.union(qset)
+            if k == 'sinks':  # 污点数据
+                templates = [
+                    r'"targetValues": ".*{}.*"', r'"sourceValues": ".*{}.*"'
+                ]
+                searchfields_.extend(
+                    map(lambda x: ('method_pool', x.format(v), templates)))
+            elif k == 'signature':  # 方法签名
+                templates = [r'"signature": ".*{}.*"']
+                searchfields_.extend(
+                    map(lambda x: ('method_pool', x.format(v), templates)))
+            else:
+                searchfields_.append((k, v))
+        q = reduce(
+            lambda x, y: x | y,
+            map(
+                lambda x: Q(**x),
+                map(
+                    lambda kv_pair:
+                    {'__'.join([kv_pair[0], 'regex']): kv_pair[1]},
+                    searchfields_)), Q())
         queryset = MethodPool.objects.filter(q).order_by('-create_time').all()
         page_summary, method_pools = self.get_paginator(
             queryset, page, page_size)
