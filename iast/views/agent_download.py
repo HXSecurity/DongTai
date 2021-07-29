ac1ed286476e067fe458ba2b623c59deadad8eb4
sjh <sjh@jindong-auto.com>
2021-07-29 16:34:34 +0800

    add python agent download

--- iast/views/agent_download.py
+++ iast/views/agent_download.py
@@ -31,22 +31,29 @@ class AgentDownload(UserEndPoint):
         """
         try:
             base_url = request.query_params.get('url', 'https://www.huoxian.cn')
-            jdk_version = request.query_params.get('jdk.version', 'https://www.huoxian.cn')
+            langguage = request.query_params.get('langguage', 'java')
             project_name = request.query_params.get('projectName', 'Demo Project')
             token, success = Token.objects.values('key').get_or_create(user=request.user)
             resp = requests.get(
-                url=f'{AGENT_SERVER_PROXY["HOST"]}/api/v1/agent/download?url={base_url}&jdk.version={jdk_version}&projectName={project_name}',
+                url=f'{AGENT_SERVER_PROXY["HOST"]}/api/v1/agent/download?url={base_url}&langguage={langguage}&projectName={project_name}',
                 headers={
                     'Authorization': f'Token {token["key"]}'
                 })
             # 创建文件
-            temp_filename = f'temp/agent-{token["key"]}.jar'
-            with open(temp_filename, 'wb') as f:
-                f.write(resp.content)
-
-            response = FileResponse(open(temp_filename, 'rb'))
-            response['content_type'] = 'application/octet-stream'
-            response['Content-Disposition'] = "attachment; filename=agent.jar"
+            if langguage == "java":
+                temp_filename = f'temp/agent-{token["key"]}.jar'
+                with open(temp_filename, 'wb') as f:
+                    f.write(resp.content)
+                response = FileResponse(open(temp_filename, 'rb'))
+                response['content_type'] = 'application/octet-stream'
+                response['Content-Disposition'] = "attachment; filename=agent.jar"
+            else:
+                temp_filename = f'temp/dongtai-agent-python-{token["key"]}.tar.gz'
+                with open(temp_filename, 'wb') as f:
+                    f.write(resp.content)
+                response = FileResponse(open(temp_filename, 'rb'))
+                response['content_type'] = 'application/octet-stream'
+                response['Content-Disposition'] = "attachment; filename=dongtai-agent-python.tar.gz"
             os.remove(temp_filename)
             return response
         except Exception as e:
