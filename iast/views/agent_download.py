1d95f00e6afe6a13cb932a37ab33334f80ee103d
sjh <sjh@jindong-auto.com>
2021-07-30 12:20:34 +0800

    debug webapi agent download

--- iast/views/agent_download.py
+++ iast/views/agent_download.py
@@ -36,14 +36,14 @@ class AgentDownload(UserEndPoint):
             }
         }
 
-    def res_by_langguage(self,langguage, token, resp):
-        temp_filename = f'temp/dongtai-agent-{langguage}-{token["key"]}.{self.common_info[langguage]["extension"]}'
+    def res_by_language(self,language, token, resp):
+        temp_filename = f'temp/dongtai-agent-{language}-{token["key"]}.{self.common_info[language]["extension"]}'
         with open(temp_filename, 'wb') as f:
             f.write(resp.content)
         response = FileResponse(open(temp_filename, 'rb'))
         response['content_type'] = 'application/octet-stream'
 
-        response['Content-Disposition'] = "attachment; filename={}".format(self.common_info[langguage]['filename'])
+        response['Content-Disposition'] = "attachment; filename={}".format(self.common_info[language]['filename'])
         os.remove(temp_filename)
         return response
 
@@ -55,17 +55,17 @@ class AgentDownload(UserEndPoint):
         """
         # try:
         base_url = request.query_params.get('url', 'https://www.huoxian.cn')
-        langguage = request.query_params.get('langguage', 'java')
+        language = request.query_params.get('language', 'java')
         project_name = request.query_params.get('projectName', 'Demo Project')
         token, success = Token.objects.values('key').get_or_create(user=request.user)
         resp = requests.get(
-            url=f'{AGENT_SERVER_PROXY["HOST"]}/api/v1/agent/download?url={base_url}&langguage={langguage}&projectName={project_name}',
+            url=f'{AGENT_SERVER_PROXY["HOST"]}/api/v1/agent/download?url={base_url}&language={language}&projectName={project_name}',
             headers={
                 'Authorization': f'Token {token["key"]}'
             })
         # 创建文件
-        response = self.res_by_langguage(langguage, token, resp)
-        print(response)
+        response = self.res_by_language(language, token, resp)
+
         return response
         # except Exception as e:
         #     logger.error(e)
