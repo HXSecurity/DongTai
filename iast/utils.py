4e120e98f2950837ee27ab33ab70b19c5a66d87d
bidaya0 <xianzelin984@outlook.com>
2021-07-31 15:50:03 +0800

    extract functions in engine_method_pool_search

--- iast/utils.py
+++ iast/utils.py
@@ -5,7 +5,9 @@
 # software: Vim8
 # project: webapi
 
-
+from functools import reduce
+from django.db.models import Q
+import operator
 
 def get_model_field(model, exclude=[], include=[]):
     fields = [field.name for field in model._meta.fields]
@@ -15,3 +17,18 @@ def get_model_field(model, exclude=[], include=[]):
             if field in include
         ]
     return list(set(fields) - set(exclude))
+
+
+def assemble_query(condictions: dict,
+                   lookuptype='',
+                   base_query=Q(),
+                   operator_=operator.or_):
+    return reduce(
+        operator_,
+        map(
+            lambda x: Q(**x),
+            map(
+                lambda kv_pair: {
+                    '__'.join(filter(lambda x: x, [kv_pair[0], lookuptype])):
+                    kv_pair[1]
+                }, condictions)), base_query)
