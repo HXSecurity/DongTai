ca82034e66a7e580bddfb058d32062e243dd6e5d
bidaya0 <xianzelin984@outlook.com>
2021-08-12 10:29:04 +0800

    add doc

--- iast/views/engine_method_pool_search.py
+++ iast/views/engine_method_pool_search.py
@@ -8,12 +8,19 @@ from dongtai.models.project import IastProject
 from dongtai.models.user import User
 from dongtai.models.vulnerablity import IastVulnerabilityModel
 
-from iast.utils import get_model_field, assemble_query
+from iast.utils import get_model_field, assemble_query,extend_schema_with_envcheck
 import re
 import operator
 
 
 class MethodPoolSearchProxy(AnonymousAndUserEndPoint):
+    @extend_schema_with_envcheck([{
+        'name': "page_size",
+        "type": int,
+    }, {
+        'name': "page_index",
+        "type": int,
+    }],{})
     def get(self, request):
         page_size = int(request.query_params.get('page_size', 1))
         page = request.query_params.get('page_index', 1)
