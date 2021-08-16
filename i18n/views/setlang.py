aed085d883f6d4b59a374719abf87ee7e2d52931
bidaya0 <xianzelin984@outlook.com>
2021-08-16 16:25:18 +0800

    fix

--- i18n/views/setlang.py
+++ i18n/views/setlang.py
@@ -14,7 +14,7 @@ LANGUAGE_QUERY_PARAMETER = 'language'
 class LanguageSetting(AnonymousAndUserEndPoint):
     def get(self, request):
         lang_code = request.GET.get(LANGUAGE_QUERY_PARAMETER)
-        response = JsonResponse({})
+        response = JsonResponse({'status': 201})
         response.set_cookie(
             settings.LANGUAGE_COOKIE_NAME,
             lang_code,
