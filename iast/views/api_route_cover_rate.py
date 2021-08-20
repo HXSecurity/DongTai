aab98f8dcd9dcac96dee10b6524fa875be8b9c78
bidaya0 <xianzelin984@outlook.com>
2021-08-20 16:39:53 +0800

    api route cover rate

---
+++ iast/views/api_route_cover_rate.py
@@ -0,0 +1,40 @@
+######################################################################
+# @author      : bidaya0 (bidaya0@$HOSTNAME)
+# @file        : api_route_cover_rate
+# @created     : Friday Aug 20, 2021 16:20:10 CST
+#
+# @description :
+######################################################################
+
+from iast.base.project_version import get_project_version, get_project_version_by_id
+from dongtai.models.agent import IastAgent
+from dongtai.endpoint import R, UserEndPoint
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+from iast.utils import checkcover, batch_queryset
+
+
+class ApiRouteSearch(UserEndPoint):
+    def get(self, request):
+        project_id = request.query_params.get('project_id', None)
+        version_id = request.query_params.get('version_id', None)
+        auth_users = self.get_auth_users(request.user)
+        if not version_id:
+            current_project_version = get_project_version(
+                project_id, auth_users)
+        else:
+            current_project_version = get_project_version_by_id(version_id)
+        agents = IastAgent.objects.filter(
+            user__in=auth_users,
+            bind_project_id=project_id,
+            project_version_id=current_project_version.get("version_id",
+                                                           0)).values("id")
+        q = Q(agent_id__in=[_['id'] for _ in agents])
+        total = q.count()
+        cover_count = 0
+        for api_route in batch_queryset(q):
+            if checkcover(api_route):
+                cover_count += 1
+        cover_rate = "{.2f}%".format(cover_count / total)
+        return R.success(msg=_('API coverage rate obtained successfully'),
+                         data={'cover_rate': cover_rate})
--- iast/views/api_route_search.py
+++ iast/views/api_route_search.py
@@ -14,6 +14,7 @@ from iast.base.project_version import get_project_version, get_project_version_b
 import hashlib
 from dongtai.models.agent_method_pool import MethodPool
 from django.forms.models import model_to_dict
+from iast.utils import checkcover, batch_queryset
 
 
 class ApiRouteSearch(UserEndPoint):
@@ -68,7 +69,7 @@ class ApiRouteSearch(UserEndPoint):
 def _filter_and_label(api_routes, limit, is_cover=None):
     api_routes_after_filter = []
     for api_route in batch_queryset(api_routes):
-        api_route.is_cover = _checkcover(api_route)
+        api_route.is_cover = checkcover(api_route)
         if is_cover:
             api_routes_after_filter += [
                 api_route
@@ -80,14 +81,6 @@ def _filter_and_label(api_routes, limit, is_cover=None):
     return api_routes_after_filter
 
 
-def batch_queryset(queryset, batch_size=1):
-    iter_ = 0
-    while True:
-        queryset = list(queryset[iter_:iter_ + 1])
-        if not queryset:
-            break
-        else:
-            yield queryset[0]
 
 
 def _serialize(api_route, agents, http_method):
@@ -108,16 +101,3 @@ def _get_responses(api_route):
     return [model_to_dict(response) for response in responses]
 
 
-def _checkcover(api_route, agents, http_method):
-    uri_hash = hashlib.sha1(api_route.uri.encode('utf-8')).hexdigest()
-    api_method_id = api_route.values('method_id')
-    http_method_ids = IastApiMethodHttpMethodRelation.objects.filter(
-        api_method_id=api_method_id).values('api_method_id')
-    http_methods = HttpMethod.objects.filter(
-        http_method_ids__in=http_method_ids).all().values_list('method')
-    q = Q(agent_id__in=[_['id'] for _ in agents])
-    q = q & Q(http_method__in=http_methods)
-    q = q & Q(uri_hash=uri_hash)
-    if MethodPool.objects.filter(q)[0:1]:
-        return True
-    return False
