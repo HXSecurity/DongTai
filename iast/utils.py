6e541375dc5d848a188735fc8a20bdf4e8f2e3b4
bidaya0 <xianzelin984@outlook.com>
2021-08-20 16:39:53 +0800

    api route cover rate

--- iast/utils.py
+++ iast/utils.py
@@ -8,6 +8,9 @@
 from functools import reduce
 from django.db.models import Q
 import operator
+import hashlib
+from dongtai.models.api_route import IastApiRoute, IastApiMethod, IastApiRoute, HttpMethod, IastApiResponse, IastApiMethodHttpMethodRelation
+from dongtai.models.agent_method_pool import MethodPool
 
 
 def get_model_field(model, exclude=[], include=[]):
@@ -33,3 +36,29 @@ def assemble_query(condictions: dict,
                     '__'.join(filter(lambda x: x, [kv_pair[0], lookuptype])):
                         kv_pair[1]
                 }, condictions)), base_query)
+
+
+
+def batch_queryset(queryset, batch_size=1):
+    iter_ = 0
+    while True:
+        queryset = list(queryset[iter_:iter_ + 1])
+        if not queryset:
+            break
+        else:
+            yield queryset[0]
+
+
+def checkcover(api_route, agents, http_method):
+    uri_hash = hashlib.sha1(api_route.uri.encode('utf-8')).hexdigest()
+    api_method_id = api_route.values('method_id')
+    http_method_ids = IastApiMethodHttpMethodRelation.objects.filter(
+        api_method_id=api_method_id).values('api_method_id')
+    http_methods = HttpMethod.objects.filter(
+        http_method_ids__in=http_method_ids).all().values_list('method')
+    q = Q(agent_id__in=[_['id'] for _ in agents])
+    q = q & Q(http_method__in=http_methods)
+    q = q & Q(uri_hash=uri_hash)
+    if MethodPool.objects.filter(q)[0:1]:
+        return True
+    return False
