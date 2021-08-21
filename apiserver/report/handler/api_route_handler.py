9df2bd2eafbfee6690ce1f5228714c243a043c92
bidaya0 <xianzelin984@outlook.com>
2021-08-21 11:22:40 +0800

    api route

--- apiserver/report/handler/api_route_handler.py
+++ apiserver/report/handler/api_route_handler.py
@@ -23,40 +23,41 @@ logger = logging.getLogger('dongtai.openapi')
 class ApiRouteHandler(IReportHandler):
     def parse(self):
         self.api_data = self.detail.get('api_data')
-        self.api_routes = map(lambda x: _data_dump(x, self.api_data))
+        self.api_routes = map(lambda x: _data_dump(x),self.api_data)
 
     def save(self):
         try:
             agent = IastAgent.objects.filter(pk=self.agent_id)[0:1]
             if not agent:
                 raise ValueError(_("No such agent"))
+            agent=agent[0]
             for api_route in self.api_routes:
                 http_methods = []
                 for http_method in api_route['method']:
-                    http_method, __ = HttpMethod.get_or_create(
+                    http_method, __ = HttpMethod.objects.get_or_create(
                         method=http_method.upper())
                     http_methods.append(http_method)
-                api_method, is_create = IastApiMethod.get_or_create(
+                api_method, is_create = IastApiMethod.objects.get_or_create(
                     method='/'.join(api_route['method']))
                 if is_create:
                     for http_method in http_methods:
-                        IastApiMethodHttpMethodRelation.create(
+                        IastApiMethodHttpMethodRelation.objects.create(
                             api_method_id=api_method.id,
                             http_method_id=http_method.id)
                 fields = [
-                    'path', 'code_class', 'description', 'code_file',
+                    'uri', 'code_class', 'description', 'code_file',
                     'controller', 'agent'
                 ]
                 api_route_dict = _dictfilter(api_route, fields)
                 api_route_obj = _route_dump(api_route_dict, api_method, agent)
-                api_route = api_route.create(**api_route_obj)
+                api_route_model = IastApiRoute.objects.create(**api_route_obj)
                 parameters = api_route['parameters']
                 for parameter in parameters:
-                    parameter_obj = _para_dump(parameter, api_route)
-                    IastApiParameter.create(**parameter_obj)
+                    parameter_obj = _para_dump(parameter, api_route_model)
+                    IastApiParameter.objects.create(**parameter_obj)
                 response_obj = _response_dump(
-                    {'return_type': api_route['return_type']}, api_route)
-                IastApiResponse.create(**response_obj)
+                    {'return_type': api_route['return_type']}, api_route_model)
+                IastApiResponse.objects.create(**response_obj)
                 logger.info(_('API导航日志记录成功'))
         except Exception as e:
             logger.info(_('API导航日志失败，原因:{}').format(e))
@@ -71,17 +72,20 @@ def _data_dump(item):
 def _route_dump(item, api_method, agent):
     item['method'] = api_method
     item['agent'] = agent
+    item['path'] = item['uri']
+    del item['uri']
     return item
 
 
 def _para_dump(item, api_route):
-    item['api_route_id'] = api_route
+    item['route'] = api_route
     item['parameter_type'] = item['type']
+    del item['type']
     return item
 
 
 def _response_dump(item, api_route):
-    item['api_route_id'] = api_route
+    item['route'] = api_route
     return item
 
 
