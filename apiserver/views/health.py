7105906a7ac9ef73abd36bcb528df53b05b70ffe
bidaya0 <xianzelin984@outlook.com>
2021-08-25 19:21:49 +0800

    flake8

--- apiserver/views/health.py
+++ apiserver/views/health.py
@@ -19,29 +19,6 @@ import json
 logger = logging.getLogger("dongtai.openapi")
 
 
-class HealthView(OpenApiEndPoint):
-    def get(self, request):
-        oss_status, _ = _checkossstatus()
-        statusmap = {True: 1, False: 0}
-        _checkenginestatus, engine_resp = _checkenginestatus()
-        data = {
-            "dongtai_openapi": {
-                "status": 1
-            },
-            "oss": {
-                "status": statusmap[oss_status]
-            }
-        }
-        if engine_resp:
-            data.update(engine_resp)
-        else:
-            data.update({
-                "dongtai_engine": 0,
-                "engine_monitoring_indicators": []
-            })
-        return R.success(data=data)
-
-
 def _checkossstatus():
     try:
         auth = oss2.Auth(settings.ACCESS_KEY, settings.ACCESS_KEY_SECRET)
@@ -69,3 +46,26 @@ def _checkenginestatus():
         logger.log("HealthView_checkenginestatus:{}".format(e))
         return False, None
     return True, resp
+
+
+class HealthView(OpenApiEndPoint):
+    def get(self, request):
+        oss_status, _ = _checkossstatus()
+        statusmap = {True: 1, False: 0}
+        engine_status, engine_resp = _checkenginestatus()
+        data = {
+            "dongtai_openapi": {
+                "status": 1
+            },
+            "oss": {
+                "status": statusmap[oss_status]
+            }
+        }
+        if engine_status:
+            data.update(engine_resp)
+        else:
+            data.update({
+                "dongtai_engine": 0,
+                "engine_monitoring_indicators": []
+            })
+        return R.success(data=data)
