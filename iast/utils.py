dbcc3209e75dd048c223cc0ed0df566a1ac4c819
bidaya0 <xianzelin984@outlook.com>
2021-09-07 16:43:05 +0800

    MethodOverrideMiddleware

--- iast/utils.py
+++ iast/utils.py
@@ -147,3 +147,17 @@ def checkopenapistatus(openapiurl, token):
         logger.info("HealthView_{}:{}".format(openapiurl, e))
         return False, None
     return True, resp
+
+
+
+METHOD_OVERRIDE_HEADER = 'HTTP_X_HTTP_METHOD_OVERRIDE'
+
+class MethodOverrideMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.method == 'POST' and METHOD_OVERRIDE_HEADER in request.META:
+            request.method = request.META[METHOD_OVERRIDE_HEADER]
+        return self.get_response(request)
