22f1c61c96ad7acdf537f114c77a99e349d72d7d
bidaya0 <xianzelin984@outlook.com>
2021-09-08 19:21:59 +0800

    fix

--- iast/views/user_register_batch.py
+++ iast/views/user_register_batch.py
@@ -56,8 +56,8 @@ class UserRegisterEndPoint(SystemAdminEndPoint):
         :return:
         """
 
-        #webhook_token = settings.config.get('wenjuan_webhook', 'token')
-        if 1 or token == webhook_token:
+        webhook_token = settings.config.get('wenjuan_webhook', 'token')
+        if token == webhook_token:
             entry = request.data['entry']
             username = entry['field_1']
             phone = entry['field_2']
@@ -101,8 +101,8 @@ class UserRegisterEndPoint(SystemAdminEndPoint):
             from_addr=settings.EMAIL_FROM_ADDR,
             to_addrs=[email_addr, settings.ADMIN_EMAIL],
             _subject='【洞态IAST】账号创建成功',
-            _content=f'''<h3>洞态IAST账号创建成功</h3><span>登陆地址：</span>https://iast.io/login，账号：{username}，密码：{password}<br/>登陆之后，请马上修改默认密码，然后重新登陆使用。<br/>官方网站：https://dongtai.io<br/><img width="400px" height="400px" src="{GONG_ZHONG_HAO_IMAGE}" ><hr></hr><h3>Welcome to DongTai IAST,
-    your account has been succefully created.</h3><span>Login URL:</span>https://iast.io/login，Account：{username}，Password：{password}<br/>Notice: You MUST change the password during the first time log in. Password can be changed at "Settings/Account".  After that, you can log in again.<br/>DongTai IAST Official Website: https://dongtai.io<br/><img width="400px" height="400px" src="{GONG_ZHONG_HAO_IMAGE}">''',
+            _content=f'''<h3>洞态IAST账号创建成功</h3><span>登陆地址：</span>https://iast.io/login<br></br>账号：{username}<br></br>密码：{password}<br></br><br/>登陆之后，请马上修改默认密码，然后重新登陆使用。<br/><br></br>官方网站：https://dongtai.io<br/><br></br><br>官方公众号：洞态公众号二维码</br><img width="400px" height="400px" src="{GONG_ZHONG_HAO_IMAGE}" ><hr></hr><h3>Welcome to DongTai IAST,
+    your account has been succefully created.</h3><span>Login URL:</span>https://iast.io/login<br></br>Account：{username}<br></br>Password：{password}<br></br><br/>Notice: You MUST change the password during the first time log in. Password can be changed at "Settings/Account".  After that, you can log in again.<br/>DongTai IAST Official Website: https://dongtai.io<br/><br></br><br>DongTai IAST WeChat Official Account: DongTai IAST's QR Code </br><img width="400px" height="400px" src="{GONG_ZHONG_HAO_IMAGE}">''',
             content_type='html',
         )
         print(res)
