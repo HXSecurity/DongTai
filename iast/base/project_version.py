39496977c61292b61dc3d11c4e1e78029fb605a0
bidaya0 <xianzelin984@outlook.com>
2021-09-15 17:29:54 +0800

    apidocument

--- iast/base/project_version.py
+++ iast/base/project_version.py
@@ -4,6 +4,16 @@ from dongtai.models.project_version import IastProjectVersion
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
+class VersionModifySerializer(serializers.Serializer):
+    version_id = serializers.CharField(
+        help_text=_("The version id of the project"))
+    version_name = serializers.CharField(
+        help_text=_("The version name of the project"))
+    description = serializers.CharField(
+        help_text=_("Description of the project versoin"))
+    project_id = serializers.IntegerField(help_text=_("The id of the project"))
+    current_version = serializers.IntegerField(help_text=_(
+        "Whether it is the current version, 1 means yes, 0 means no."))
 
 
 def version_modify(user, versionData=None):
@@ -60,6 +70,7 @@ def version_modify(user, versionData=None):
             "description": description
         }
     }
+
 
 
 def get_project_version(project_id, auth_users):
