c92e78206f08113b184109f115ffd729db0bf0bb
bidaya0 <xianzelin984@outlook.com>
2021-09-17 16:15:56 +0800

    update apidocument

--- iast/views/agent_upgrade_online.py
+++ iast/views/agent_upgrade_online.py
@@ -10,11 +10,34 @@ from dongtai.endpoint import TalentAdminEndPoint, R
 from dongtai.models import User
 from django.utils.translation import gettext_lazy as _
 
+from rest_framework import serializers
+from iast.utils import extend_schema_with_envcheck, get_response_serializer
+
+
+class AgentUpgradeArgsSerializer(serializers.Serializer):
+    url = serializers.CharField(required=False)
+    token = serializers.CharField(required=False)
+
+
+_ResponseSerializer = get_response_serializer(status_msg_keypair=(
+    ((201, _('Online upgrade successful')), ''),
+    ((202,
+      _('Token verification failed, please confirm your input address and token are correct'
+        )), ''),
+))
+
 
 class AgentUpgradeOnline(TalentAdminEndPoint):
     name = "api-v1-agent-install"
     description = _("Online Upgrade Agent")
 
+
+
+    @extend_schema_with_envcheck(request=AgentUpgradeArgsSerializer,
+                                 tags=[_('Agent')],
+                                 summary=_('Agent Upgrade Online'),
+                                 description=_(""),
+                                 response_schema=_ResponseSerializer)
     def post(self, request):
         url = request.data['url']
         token = request.data['token']
