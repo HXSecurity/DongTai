11674064d994da2bca8fa878bfad3b68124ffe83
bidaya0 <xianzelin984@outlook.com>
2021-09-29 21:09:53 +0800

    fix:HXSecurity/issue/259

--- dongtai/endpoint/__init__.py
+++ dongtai/endpoint/__init__.py
@@ -168,7 +168,7 @@ class EndPoint(APIView):
         try:
             page_info.validate_number(page)
             page_list = page_info.get_page(page).object_list
-        except (EmptyPage, PageNotAnInteger, AssertionError, ZeroDivisionError):
+        except:
             return page_summary, []
         return page_summary, page_list
 
