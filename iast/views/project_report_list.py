7968492f5266bda96fced3cee329ff1b86142bd7
luzhongyang <luzhongyang@example.com>
2021-11-05 22:56:58 +0800

    search by pid

--- iast/views/project_report_list.py
+++ iast/views/project_report_list.py
@@ -3,6 +3,7 @@
 from dongtai.endpoint import R
 from dongtai.endpoint import UserEndPoint
 from dongtai.models.project_report import ProjectReport
+from dongtai.models.project import IastProject
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from iast.utils import extend_schema_with_envcheck
@@ -13,6 +14,7 @@ class _ProjectReportSearchQuerysSerializer(serializers.Serializer):
     page_size = serializers.IntegerField(default=20,
                                          help_text=_('Number per page'))
     page = serializers.IntegerField(default=1, help_text=_('Page index'))
+    pid = serializers.IntegerField(default=1, help_text=_('Project id'))
 
 
 class _ProjectReportListDataSerializer(serializers.ModelSerializer):
@@ -35,8 +37,10 @@ class ProjectReportList(UserEndPoint):
     )
     def get(self, request):
         page = request.query_params.get('page', 1)
+        pid = request.query_params.get('pid', 0)
         page_size = request.query_params.get('page_size', 20)
         ser = _ProjectReportSearchQuerysSerializer(data=request.data)
+        project = IastProject.objects.filter(pk=pid, user=request.user).first()
         try:
             if ser.is_valid(True):
                 page = ser.validated_data['page']
@@ -45,6 +49,7 @@ class ProjectReportList(UserEndPoint):
             return R.failure(data=e.detail)
         queryset = ProjectReport.objects.filter(
             user=request.user,
+            project=project
         ).order_by('-create_time')
 
         page_summary, page_data = self.get_paginator(queryset, page, page_size)
