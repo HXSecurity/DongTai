363b1b4f518caca30f92bedd6df28c19db45cf61
owefsad <dongzhiyong@secnium.cn>
2021-11-19 22:54:21 +0800

    add sensitive check logic

--- core/plugins/strategy_sensitive.py
+++ core/plugins/strategy_sensitive.py
@@ -6,23 +6,52 @@
 # desc: data rule, response field rule, sql field rule
 import re
 
+import jq
+from dongtai.models.sensitive_info import IastSensitiveInfoRule
+from dongtai.utils import const
+from celery.apps.worker import logger
+
 from core.plugins.strategy_headers import save_vul
 
 
+def parse_json_response(res_body):
+    try:
+        import json
+        return json.loads(res_body)
+    except Exception as e:
+        return None
+
+
 def check_response_content(method_pool):
-    # todo load sensitive type and rule
-    # todo match response content with sensitive rule
+    rules = IastSensitiveInfoRule.objects.filter(status=const.HOOK_TYPE_ENABLE)
+    json_response = parse_json_response(method_pool.res_body)
+    for rule in rules:
+        try:
+            if rule.pattern_type.id == 1:
+                pattern = re.compile(rule.pattern, re.M)
+                result = pattern.search(method_pool.res_body)
+                if result and result.groups():
+                    save_vul(
+                        vul_type=rule.strategy.vul_type,
+                        method_pool=method_pool,
+                        position='HTTP Response Body',
+                        data=result.group(1)
+                    )
+            elif json_response and rule.pattern_type.id == 2:
+                pattern = jq.compile(rule.pattern)
+                result = pattern.input(json_response).all()
+                if result:
+                    save_vul(
+                        vul_type=rule.strategy.vul_type,
+                        method_pool=method_pool,
+                        position='HTTP Response Body',
+                        data=' '.join(result)
+                    )
+        except Exception as e:
+            logger.error(
+                f'check_response_content error, rule: {rule.id}, rule name: {rule.strategy.vul_type}, reason: {e}')
 
-    search_phone_number_leak(method_pool)
     search_id_card_leak(method_pool)
-
-
-def search_phone_number_leak(method_pool):
-    status, phone_number = check_phone_number(method_pool.res_body)
-    if status:
-        # todo: add highlight to phone_number
-        save_vul(vul_type='Phone Number Leak', method_pool=method_pool, position='HTTP Response Body',
-                 data=phone_number)
 
 
 def search_id_card_leak(method_pool):
@@ -37,15 +66,6 @@ def search_id_card_leak(method_pool):
     if check_id_card(card):
         # todo: add highlight to id_card
         save_vul(vul_type='ID Number Leak', method_pool=method_pool, position='HTTP Response Body', data=card)
-
-
-def check_phone_number(res_body):
-    rule = '\D(1[3-9]\d{9})\D'
-    pattern = re.compile(rule, re.M)
-    result = pattern.search(res_body)
-    if result.groups():
-        return True, result.group(1)
-    return False, None
 
 
 def check_id_card(id_card):
