fa487453c1a0fd96e196690cf4f2d1a530d40033
Hardy <4044094+hardy4yooz@users.noreply.github.com>
2021-11-29 15:45:57 +0800

    Update install.sh to dtctl

--- build_with_docker_compose.sh
+++ build_with_docker_compose.sh
@@ -2,11 +2,16 @@
 
 build_dongtai_iast(){
 	echo -e "\n\033[33m[+] install DongTai IAST service, openapi service addr is $1\033[0m"
-	chmod u+x deploy/docker-compose/install.sh
+	chmod u+x deploy/docker-compose/dtctl
+
+	latest_version="`wget -qO- -t1 -T2 "https://api.github.com/repos/HXSecurity/DongTai/releases/latest" | jq -r '.tag_name'`"
+	latest_version=${latest_version:1}
+	echo  "Install version is $latest_version"
+
 	if [ $? -ne 0 ]; then
-		bash deploy/docker-compose/install.sh $1
+		./deploy/docker-compose/dtctl install -v $latest_version
 	else
-		./deploy/docker-compose/install.sh $1
+		./deploy/docker-compose/dtctl install -v $latest_version
 	fi
 	echo -e "\n\033[32m[*] start DongTai IAST service, please wait 30s\033[0m"
 }
