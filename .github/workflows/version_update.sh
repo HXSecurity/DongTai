a7d810e35686674903a557ae49610b868423f67c
Owefsad <16431735+exexute@users.noreply.github.com>
2021-12-20 17:07:07 +0800

    Fix: ubuntu sed error

--- .github/workflows/version_update.sh
+++ .github/workflows/version_update.sh
@@ -6,9 +6,9 @@ git config --global user.name "$GITHUB_ACTOR"
 git config --global user.email "$GITHUB_ACTOR@bot.noreplay.dongtai.io"
 git checkout -b "release-$NEW_VERSION"
 
-sed -i "" "s/:latest/:$NEW_VERSION/g" README.MD
-sed -i "" "s/:latest/:$NEW_VERSION/g" README_CN.MD
-sed -i "" "s/dongtai-mysql:latest/dongtai-mysql:$NEW_VERSION/g" .github/workflows/deploy_test.yml
+sed -i "s/:latest/:$NEW_VERSION/g" README.md
+sed -i "s/:latest/:$NEW_VERSION/g" README_CN.md
+sed -i "s/dongtai-mysql:latest/dongtai-mysql:$NEW_VERSION/g" .github/workflows/deploy_test.yml
 
 git add .
 git commit -m "Update: change version to $NEW_VERSION"
