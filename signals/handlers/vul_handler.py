3846e280d7274d960b101df2f843ba53f260c3a8
hzmciast <90294437+hzmciast@users.noreply.github.com>
2021-12-29 10:30:40 +0800

    Update vul_handler.py
    合并https://github.com/HXSecurity/DongTai-engine/pull/122中重复的send_to_wechat()方法

--- signals/handlers/vul_handler.py
+++ signals/handlers/vul_handler.py
@@ -426,31 +426,6 @@ def send_vul_notify(vul):
                 pass
             else:
                 pass
-            
-            
-"""
-企业微信机器人告警
-默认@所有人
-"""
-def send_to_wechat(vul):
-    wechat = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=xxxx"
-    header = {
-        "Content-Type": "application/json",
-        "Charset": "UTF-8"
-    }
-
-    message = {
-        "msgtype": "text",
-        "text": {
-            "content": "IAST漏洞扫描告警通知，请相关同事及时处理！！！\n漏洞类型：{}\n危害等级：{}\n漏洞URL：{}\n业务线名称：{}\n探针agent：{}".format(vul.strategy.vul_type, vul.level.name_value, vul.url, vul.agent.project_name, vul.agent.token),
-            "mentioned_list":["@all"],
-            "mentioned_mobile_list":["@all"]
-        },
-    }
-    
-    resp = requests.post(url=wechat, headers=header, data=json.dumps(message))
-    if resp.status_code == 200:
-        pass
     
 
 def send_to_web_hook(web_hook_url, template, vul_data):
@@ -491,9 +466,26 @@ def send_to_dingding():
     pass
 
 
-def send_to_wechat():
+def send_to_wechat(vul):
     """
     todo 发送漏洞通知到企业微信
     :return:
     """
-    pass
+    wechat = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=xxxx"
+    header = {
+        "Content-Type": "application/json",
+        "Charset": "UTF-8"
+    }
+
+    message = {
+        "msgtype": "text",
+        "text": {
+            "content": "IAST漏洞扫描告警通知，请相关同事及时处理！！！\n漏洞类型：{}\n危害等级：{}\n漏洞URL：{}\n业务线名称：{}\n探针agent：{}".format(vul.strategy.vul_type, vul.level.name_value, vul.url, vul.agent.project_name, vul.agent.token),
+            "mentioned_list":["@all"],
+            "mentioned_mobile_list":["@all"]
+        },
+    }
+    
+    resp = requests.post(url=wechat, headers=header, data=json.dumps(message))
+    if resp.status_code == 200:
+        pass
