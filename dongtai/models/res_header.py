22a05df9775a4ebaf5ddbf469325edb6960183a5
xianzelin@huoxian.cn <xianzelin@huoxian.cn>
2022-01-12 17:35:25 +0800

    feature: add res_header models for test

--- dongtai/models/res_header.py
+++ dongtai/models/res_header.py
@@ -12,13 +12,20 @@ from dongtai.utils.settings import get_managed
 from dongtai.models.agent import IastAgent
 
 
-class ProjectResHeader(models.Model):
+class HeaderType(models.IntegerChoices):
+    REQUEST = 1
+    RESPONSE = 2
+
+
+class ProjectSaasMethodPoolHeader(models.Model):
     key = models.CharField(max_length=255, blank=True, null=False)
     agent = models.ForeignKey(IastAgent,
                               models.DO_NOTHING,
                               blank=True,
                               null=True,
                               db_constraint=False)
+    header_type = models.IntegerField(choices=HeaderType.choices,default=0)
+
     class Meta:
         managed = get_managed()
-        db_table = 'iast_project_res_header'
+        db_table = 'iast_project_header'
