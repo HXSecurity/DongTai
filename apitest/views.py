2358daa88a5022a64b4538e636c460de6db90195
xianzelin@huoxian.cn <xianzelin@huoxian.cn>
2022-01-14 21:12:54 +0800

    bugfix:project add i18n msg change

--- apitest/views.py
+++ apitest/views.py
@@ -20,6 +20,8 @@ from apitest.utils import (
 )
 from dongtai.models.project import IastProject
 from dongtai.models.res_header import ProjectSaasMethodPoolHeader
+from django.utils.translation import gettext_lazy as _
+from iast.views.project_add import url_validate
 # Create your views here.
 
 
@@ -27,25 +29,29 @@ class ApiTestTriggerEndpoint(UserEndPoint):
     def get(self, request, pk):
         auth_users = self.get_auth_users(request.user)
         users = self.get_auth_users(request.user)
-        project = IastProject.objects.filter(user__in=users,
-                                             pk=pk).order_by('-latest_time').first()
+        project = IastProject.objects.filter(
+            user__in=users, pk=pk).order_by('-latest_time').first()
         if not project:
             return R.failure(msg='no project found')
-        if (project.test_req_header_key == ''
-                or project.test_req_header_value == ''
-                or project.base_url == ''):
-            return R.failure(
-                msg='Please enter the parameters required for the test first')
+        if not project.base_url:
+            return R.failure(msg=_(
+                'Please enter the parameters required for the test first'))
         agents = IastAgent.objects.filter(user__in=auth_users,
                                           bind_project_id=pk).values("id")
         q = Q(agent__in=agents)
+        if not IastApiRoute.objects.filter(q).exists():
+            return R.failure(msg='No API collected')
         api_routes = IastApiRoute.objects.filter(q).all()
         datas = [serialize(api_route) for api_route in api_routes]
         swaggerdatas = swagger_trans(datas)
-        runtest(swaggerdatas,
-                {project.test_req_header_key: project.test_req_header_value},
-                project.base_url)
-        return R.success()
+        if project.test_req_header_key == '' or project.test_req_header_value == '':
+            header_dict = {}
+        else:
+            header_dict = {
+                project.test_req_header_key: project.test_req_header_value
+            }
+        runtest(swaggerdatas, header_dict, project.base_url)
+        return R.success(msg=_('Starting API Test'))
 
 
 class ApiTestHeaderEndpoint(UserEndPoint):
@@ -54,6 +60,7 @@ class ApiTestHeaderEndpoint(UserEndPoint):
         agents = IastAgent.objects.filter(user__in=auth_users,
                                           bind_project_id=pk).values("id")
         q = Q(agent__in=agents)
-        headers = list(ProjectSaasMethodPoolHeader.objects.filter(q).values_list(
-            'key').distinct().all())
+        headers = list(
+            ProjectSaasMethodPoolHeader.objects.filter(q).values_list(
+                'key').distinct().all())
         return R.success(data=headers)
