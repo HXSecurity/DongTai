e2f2807806ea87c28315c638057ad2fd2462dfad
xianzelin@huoxian.cn <xianzelin@huoxian.cn>
2022-01-20 16:03:08 +0800

    change way to do version_update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,13 +9,14 @@
 ######################################################################
 
 
-MYSQL_HOST=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^host/ {print $2}'`
-MYSQL_NAME=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^name/ {print $2}'`
-MYSQL_USER=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^user/ {print $2}'`
-MYSQL_PASSWORD=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^password/ {print $2}'`
-MYSQL_PORT=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^port/ {print $2}'`
+#MYSQL_HOST=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^host/ {print $2}'`
+#MYSQL_NAME=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^name/ {print $2}'`
+#MYSQL_USER=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^user/ {print $2}'`
+#MYSQL_PASSWORD=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^password/ {print $2}'`
+#MYSQL_PORT=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^port/ {print $2}'`
+#
+#mysql --host=$MYSQL_HOST -u$MYSQL_USER -P$MYSQL_PORT -p$MYSQL_PASSWORD -D $MYSQL_NAME < /opt/dongtai/webapi/docker/version.sql || true 
 
-mysql --host=$MYSQL_HOST -u$MYSQL_USER -P$MYSQL_PORT -p$MYSQL_PASSWORD -D $MYSQL_NAME < /opt/dongtai/webapi/docker/version.sql || true 
-
+python /opt/dongtai/webapi/docker/version_update.py || true
 
 /usr/local/bin/uwsgi --ini /opt/dongtai/webapi/conf/uwsgi.ini
