08f106b48d8d907d8ac8a76c48f4db41e6101808
bidaya0 <xianzelin@huoxian.cn>
2022-02-15 15:46:46 +0800

    optimise:apiroute cover rate query optimise

--- iast/views/api_route_cover_rate.py
+++ iast/views/api_route_cover_rate.py
@@ -11,7 +11,7 @@ from dongtai.models.agent import IastAgent
 from dongtai.endpoint import R, UserEndPoint
 from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
-from iast.utils import checkcover, batch_queryset
+from iast.utils import batch_queryset, checkcover_batch
 from iast.utils import extend_schema_with_envcheck
 from dongtai.models.api_route import IastApiRoute
 from iast.utils import extend_schema_with_envcheck, get_response_serializer
@@ -56,13 +56,10 @@ class ApiRouteCoverRate(UserEndPoint):
             bind_project_id=project_id,
             project_version_id=current_project_version.get("version_id",
                                                            0)).values("id")
-        q = Q(agent_id__in=[_['id'] for _ in agents])
+        q = Q(agent__in=agents)
         queryset = IastApiRoute.objects.filter(q)
         total = queryset.count()
-        cover_count = 0
-        for api_route in batch_queryset(queryset):
-            if checkcover(api_route,agents):
-                cover_count += 1
+        cover_count = checkcover_batch(queryset,agents) 
         try:
             cover_rate = "{:.2%}".format(cover_count / total)
         except ZeroDivisionError as e:
