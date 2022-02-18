028f545916c575e4cdd50c33cacfa9df86538816
bidaya0 <bidaya0@bidaya0-x230.xianzelin.top>
2022-02-18 16:26:56 +0800

    update agent and add agent version

--- AgentServer/settings.py
+++ AgentServer/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = random.choices(string.ascii_letters + string.digits, k=50)
 DEBUG = os.environ.get("debug", 'false') == 'true'
 
 ALLOWED_HOSTS = ['*']
-
+VERSION = '1.3.1'
 # Application definition
 
 INSTALLED_APPS = [
