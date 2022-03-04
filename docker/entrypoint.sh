2a3d5303d440a45e7c08694e4782dd1a57d368ca
bidaya0 <xianzelin@huoxian.cn>
2022-03-04 15:17:27 +0800

    merge engine

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,14 @@
-#!/usr/bin/env sh
+python /opt/dongtai/engine/docker/version_update.py || true
+echo '启动uwsgi服务'
+nohup /usr/local/bin/uwsgi --ini /opt/dongtai/engine/conf/uwsgi.ini &
+sleep 2
 
-######################################################################
-# @author      : bidaya0 (bidaya0@$HOSTNAME)
-# @file        : entrypoint
-# @created     : 星期四 1月 20, 2022 15:07:37 CST
-#
-# @description : 
-######################################################################
+echo $1
 
-
-#MYSQL_HOST=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^host/ {print $2}'`
-#MYSQL_NAME=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^name/ {print $2}'`
-#MYSQL_USER=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^user/ {print $2}'`
-#MYSQL_PASSWORD=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^password/ {print $2}'`
-#MYSQL_PORT=`awk '/\[mysql\]/{flag=1;next}/\[.*\]/{flag=0}flag && NF' /opt/dongtai/webapi/conf/config.ini  | awk -F "=" '/^port/ {print $2}'`
-#
-#mysql --host=$MYSQL_HOST -u$MYSQL_USER -P$MYSQL_PORT -p$MYSQL_PASSWORD -D $MYSQL_NAME < /opt/dongtai/webapi/docker/version.sql || true 
-
-python /opt/dongtai/webapi/docker/version_update.py || true
-
-/usr/local/bin/uwsgi --ini /opt/dongtai/webapi/conf/uwsgi.ini
+if [ -z $1 ]; then
+  echo '启动Celery Worker服务'
+  celery -A lingzhi_engine worker -l info -E --pidfile=
+else
+  echo '启动Celery Beat服务'
+  celery -A lingzhi_engine beat -l info --pidfile= --scheduler django_celery_beat.schedulers:DatabaseScheduler
+fi
