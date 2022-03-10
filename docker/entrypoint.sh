1048836af16279c850eae02a907831874c442966
bidaya0 <xianzelin@huoxian.cn>
2022-03-10 17:07:47 +0800

    change docker-compose.yml

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,8 +6,10 @@ sleep 2
 echo $1
 
 if [ "$1" == "worker" ]; then
+	nohup /usr/local/bin/uwsgi --ini /opt/dongtai/webapi/conf/uwsgi.ini &
 	celery -A lingzhi_engine worker -l info -E --pidfile=
 elif [ "$1" == "beat" ]; then
+	nohup /usr/local/bin/uwsgi --ini /opt/dongtai/webapi/conf/uwsgi.ini & 
   celery -A lingzhi_engine beat -l info --pidfile= --scheduler django_celery_beat.schedulers:DatabaseScheduler
 else
 	/usr/local/bin/uwsgi --ini /opt/dongtai/webapi/conf/uwsgi.ini 
