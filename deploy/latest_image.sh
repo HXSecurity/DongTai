cafd3724657af18522cdc3ea68c5c29c7a44f40d
MA HEI <4044094+hardy4yooz@users.noreply.github.com>
2022-04-12 14:39:59 +0800

    fix:add upgrade and install for latest image

---
+++ deploy/latest_image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+images=("dongtai-server" "dongtai-web" "dongtai-mysql" "dongtai-redis")
+for image in ${images[*]}; do
+     tags=`wget -q https://registry.hub.docker.com/v1/repositories/dongtai/${image}/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | grep -E '^([0-9]+\.){0,2}(\*|[0-9]+)$' | tail -n 1 `
+     echo "$image"
+     echo -e "\t dongtai/$image:$tags"
+     echo -e "\t registry.cn-beijing.aliyuncs.com/huoxian_pub/$image:$tags"
+done
