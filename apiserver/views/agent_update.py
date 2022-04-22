9f53244164972352becba27c10817e0154d35b88
bidaya0 <xianzelin@huoxian.cn>
2022-04-22 16:53:39 +0800

    feature:add ip parse in server addr

--- apiserver/views/agent_update.py
+++ apiserver/views/agent_update.py
@@ -7,6 +7,8 @@ from apiserver.decrypter import parse_data
 from drf_spectacular.utils import extend_schema
 from dongtai.models.server import IastServer
 from django.utils.translation import gettext_lazy as _
+from urllib.parse import urlparse, urlunparse
+from iast.views.project_add import is_ip
 logger = logging.getLogger('dongtai.openapi')
 
 
@@ -27,6 +29,11 @@ class AgentUpdateEndPoint(OpenApiEndPoint):
         except Exception as e:
             logger.info(e, exc_info=True)
             return R.failure(msg="参数错误")
+        logger.info(f"agent_id:{agent_id} update_fields:{param}")
+        ip = ''
+        parse_re = urlparse(server_addr)
+        if parse_re.hostname and is_ip(parse_re.hostname):
+            ip = parse_re.hostname
         user = request.user
         agent = IastAgent.objects.filter(id=agent_id, user=user).first()
         if not agent:
@@ -36,11 +43,13 @@ class AgentUpdateEndPoint(OpenApiEndPoint):
             if not server:
                 return R.failure(msg="agent no register")
             else:
-                update_fields = ['ip', "port", 'update_time']
+                update_fields = ["port", 'update_time']
                 if protocol:
                     server.protocol = protocol
                     update_fields.append('protocol')
-                server.ip = server_addr
+                if ip:
+                    server.ip = ip
+                    update_fields.append('ip')
                 server.port = server_port
                 server.update_time = int(time.time())
                 server.save(update_fields=update_fields)
