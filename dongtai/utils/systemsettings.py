fec89bf62cdcbc6148f982c14ca1326043e058fe
bidaya0 <xianzelin@huoxian.cn>
2022-04-22 18:10:32 +0800

    feature: add global threshold config

--- dongtai/utils/systemsettings.py
+++ dongtai/utils/systemsettings.py
@@ -3,7 +3,7 @@
 # @file        : systemsettings
 # @created     : 星期二 11月 30, 2021 19:54:50 CST
 #
-# @description : 
+# @description :
 ######################################################################
 
 
@@ -16,3 +16,9 @@ def get_vul_validate():
     vul_verifiy = IastProfile.objects.filter(key='vul_verifiy').values_list(
         'value', flat=True).first()
     return True if not vul_verifiy or vul_verifiy == "1" else False
+
+
+def get_circuit_break():
+    circuit_break = IastProfile.objects.filter(
+        key='circuit_break').values_list('value', flat=True).first()
+    return True if not circuit_break or circuit_break == "1" else False
