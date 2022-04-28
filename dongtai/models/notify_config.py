1d9714e58119e66a6caa60e4195281390e5a6900
jinghao <songjinghao2012@163.com>
2022-04-28 15:10:35 +0800

    add notify config

--- dongtai/models/notify_config.py
+++ dongtai/models/notify_config.py
@@ -9,29 +9,38 @@ from dongtai.models import User
 from dongtai.utils.settings import get_managed
 
 WEB_HOOK = 1
-DING_DING = 2
+GITLAB = 2
 JIRA = 3
-EMAIL = 4
+ZENDAO = 4
+FEISHU = 5
+WEIXIN = 6
+DING_DING = 7
+
+
 
 NOTIFY_TYPE_CHOICES = (
     (WEB_HOOK, WEB_HOOK),
-    (DING_DING, DING_DING),
+    (GITLAB, GITLAB),
     (JIRA, JIRA),
-    (EMAIL, EMAIL),
+    (ZENDAO, ZENDAO),
+    (FEISHU, FEISHU),
+    (WEIXIN, WEIXIN),
+    (DING_DING, DING_DING),
 )
 
 
 class IastNotifyConfig(models.Model):
     WEB_HOOK = WEB_HOOK
     DING_DING = DING_DING
-    JIRA = JIRA
-    EMAIL = EMAIL
-
+    FEISHU = FEISHU
+    WEIXIN = WEIXIN
     NOTIFY_TYPE_CHOICES = NOTIFY_TYPE_CHOICES
 
-    notify_type = models.IntegerField(blank=True, null=True, choices=NOTIFY_TYPE_CHOICES)
-    notify_metadata = models.TextField(blank=True, null=True)  # This field type is a guess.
+    notify_type = models.SmallIntegerField(blank=True, null=True, choices=NOTIFY_TYPE_CHOICES)
+    notify_meta_data = models.TextField(blank=True, null=True)  # This field type is a guess.
     user = models.ForeignKey(to=User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    test_result = models.SmallIntegerField(blank=True, null=True)
+    create_time = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = get_managed()
