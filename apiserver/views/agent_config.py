fce14d93725fe586e712ea093b5e729537806a97
bidaya0 <xianzelin@huoxian.cn>
2022-05-08 01:31:04 +0800

    feature/agent_config_v2 base

--- apiserver/views/agent_config.py
+++ apiserver/views/agent_config.py
@@ -8,6 +8,8 @@ import logging
 from dongtai.utils.systemsettings import get_circuit_break
 from django.utils.translation import gettext_lazy as _
 from result import Ok, Err, Result
+from dongtai.models.agent_config import MetricGroup
+
 logger = logging.getLogger('dongtai.openapi')
 
 
@@ -16,8 +18,7 @@ class AgentConfigView(OpenApiEndPoint):
     @extend_schema(
         description='Through agent_ Id get disaster recovery strategy',
         responses=R,
-        methods=['POST']
-    )
+        methods=['POST'])
     def post(self, request):
         try:
             # agent_id = request.data.get('agentId', None)
@@ -41,13 +42,22 @@ class AgentConfigView(OpenApiEndPoint):
                     cluster_name__in=('', server.cluster_name),
                     cluster_version__in=('', server.cluster_version),
                     hostname__in=('', server.hostname),
-                    ip__in=('', server.ip)
-                ).order_by('-priority').first()
+                    ip__in=('', server.ip)).order_by('-priority').first()
                 if config:
                     data = config.details
 
         return R.success(data=data)
 
+from dongtai.models.agent_config import (
+    IastCircuitTarget,
+    IastCircuitConfig,
+    IastCircuitMetric,
+    TargetType,
+    Operator,
+    DealType,
+    MetricType,
+    MetricGroup,
+)
 
 class AgentConfigv2View(OpenApiEndPoint):
 
@@ -70,60 +80,82 @@ class AgentConfigv2View(OpenApiEndPoint):
         return R.success(msg=_('Successfully'), data=agent_config)
 
 
+from django.db.models import F
+
+
+def get_agent_filter_details(agent_id):
+    return IastAgent.objects.filter(pk=agent_id).values(
+        "bind_project__name", "user__username", "server__protocol", "token",
+        "server__ip", "server__path", "server__port", "language").annotate(
+            PROJECT_NAME=F("bind_project__name"),
+            ACCOUNT_NAME=F("user__username"),
+            PROTOCOL=F("server__protocol"),
+            AGENT_IP=F("server__ip"),
+            AGENT_NAME=F("alias"),
+            AGENT_PATH=F("server__path"),
+            PORT=F("server__port"),
+            AGENT_LANGUAGE=F("language"),
+        ).first()
+
+
+def get_agent_config_by_scan(agent_id: int, mg: MetricGroup) -> Result:
+    agent_detail = get_agent_filter_details(agent_id)
+    queryset = IastCircuitConfig.objects.filter(
+        is_deleted=0).order_by('-priority').only('id')
+    for i in queryset:
+        result_list = []
+        for target in IastCircuitTarget.objects.filter(
+                circuit_config_id=i.id).all():
+            result_list.append(get_filter_by_target(target)(agent_detail))
+        if all(result_list):
+            return Ok(i.id)
+    return Err()
+
+
+def get_function(opt: Operator):
+    if opt == Operator.EQUAL:
+        return lambda x, y: x == y
+    if opt == Operator.NOT_EQUAL:
+        return lambda x, y: x != y
+    if opt == Operator.CONTAIN:
+        return lambda x, y: x in y
+    if opt == Operator.NOT_CONTAIN:
+        return lambda x, y: x not in y
+
+def get_filter_by_target(target):
+    targetattr = TargetType(target.target_type).name
+    opt_function = get_function(TargetType(target.opt))
+    return lambda x:opt_function(x[targetattr], target.value)
+
 def get_agent_config(agent_id: int) -> Result:
     data = {
-        "enableAutoFallback":
-        True,
-        "performanceLimitRiskMaxMetricsCount":
-        30,
-        "systemIsUninstall":
-        True,
-        "jvmIsUninstall":
-        True,
-        "applicationIsUninstall":
-        True,
-        "system": [{
-            "fallbackName": "cpuUsagePercentage",
-            "conditions": "greater",
-            "value": 100
-        }, {
-            "fallbackName": "sysMemUsagePercentage",
-            "conditions": "greater",
-            "value": 100
-        }, {
-            "fallbackName": "sysMemUsageUsed",
-            "conditions": "greater",
-            "value": 100000000000
-        }],
-        "jvm": [{
-            "fallbackName": "jvmMemUsagePercentage",
-            "conditions": "greater",
-            "value": 100
-        }, {
-            "fallbackName": "jvmMemUsageUsed",
-            "conditions": "greater",
-            "value": 100000000000
-        }, {
-            "fallbackName": "threadCount",
-            "conditions": "greater",
-            "value": 100000
-        }, {
-            "fallbackName": "daemonThreadCount",
-            "conditions": "greater",
-            "value": 1000000
-        }, {
-            "fallbackName": "dongTaiThreadCount",
-            "conditions": "greater",
-            "value": 1000000
-        }],
-        "application": [{
-            "fallbackName": "hookLimitTokenPerSecond",
-            "conditions": "greater",
-            "value": 10000
-        }, {
-            "fallbackName": "heavyTrafficLimitTokenPerSecond",
-            "conditions": "greater",
-            "value": 100000000
-        }]
+        "enableAutoFallback": True,
+        "performanceLimitRiskMaxMetricsCount": 30,
     }
+    interval_list = []
+    for mg in MetricGroup:
+        res = get_agent_config_by_scan(agent_id, mg)
+        if isinstance(res,Err):
+            continue
+        config_id = res.value
+        config = IastCircuitConfig.objects.filter(
+            pk=config_id).order_by('-priority').first()
+        metric_list = []
+        for metric in IastCircuitMetric.objects.filter(
+                circuit_config_id=config.id).all():
+            metric_list.append(convert_metric(metric))
+        data[mg.label.lower()] = metric_list
+        data[mg.label.lower() +
+             "IsUninstall"] = True if config.deal == DealType.UNLOAD else False
+        interval_list.append(config.interval)
+    data["performanceLimitRiskMaxMetricsCount"] = min(interval_list)
     return Ok(data)
+
+
+
+def convert_metric(metric):
+    return {
+        "fallbackName": MetricType(metric.metric_type).name,
+        "conditions": Operator(metric.opt).name.lower(),
+        "value": metric.value
+    }
---
+++ test/apiserver/test_config.py
@@ -0,0 +1,18 @@
+
+from rest_framework.test import APITestCase
+from apiserver.views.agent_config import get_agent_config
+from apiserver.views.agent_config import * 
+
+class VulDetailTestCase(APITestCase):
+
+    def test_agent_config_generate(self):
+         print(get_agent_config(1))
+    
+    def test_agent_detail_retrieve(self):
+        res =get_agent_filter_details(1)
+        print(res)
+
+
+    def test_target_filter(self):
+        res =get_agent_config_by_scan(1) 
+        print(res)
