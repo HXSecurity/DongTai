dd2b569608ab5ccc24e85b289e923dd044280b16
bidaya0 <xianzelin@huoxian.cn>
2022-05-08 11:30:45 +0800

    change value field to varchar(200)

--- test/apiserver/test_config.py
+++ test/apiserver/test_config.py
@@ -1,18 +1,17 @@
-
 from rest_framework.test import APITestCase
 from apiserver.views.agent_config import get_agent_config
-from apiserver.views.agent_config import * 
+from apiserver.views.agent_config import *
+
 
 class VulDetailTestCase(APITestCase):
 
     def test_agent_config_generate(self):
-         print(get_agent_config(1))
-    
+        print(get_agent_config(1))
+
     def test_agent_detail_retrieve(self):
-        res =get_agent_filter_details(1)
+        res = get_agent_filter_details(1)
         print(res)
 
-
     def test_target_filter(self):
-        res =get_agent_config_by_scan(1) 
+        res = get_agent_config_by_scan(1, 2)
         print(res)
