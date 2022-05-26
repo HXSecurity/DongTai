f95c8ddfab19c7c91ecf4cb93bd9fa77b71eb917
bidaya0 <xianzelin@huoxian.cn>
2022-05-26 16:17:18 +0800

    deploy

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -6,12 +6,12 @@ python /opt/dongtai/webapi/deploy/docker/version_update.py || true
 echo $1
 
 if [ "$1" = "worker" ]; then
-	nohup /usr/local/bin/uwsgi --ini /opt/dongtai/webapi/conf/uwsgi.ini &
-	celery -A webapi worker -l info -E --pidfile=
+	nohup /usr/local/bin/uwsgi --ini /opt/dongtai/webapi/dongtai_conf/conf/uwsgi.ini &
+	celery -A dongtai_conf worker -l info -E --pidfile=
 elif [ "$1" = "beat" ]; then
-	nohup /usr/local/bin/uwsgi --ini /opt/dongtai/webapi/conf/uwsgi.ini  &
-  celery -A webapi beat -l info --pidfile= --scheduler django_celery_beat.schedulers:DatabaseScheduler
+	nohup /usr/local/bin/uwsgi --ini /opt/dongtai/webapi/dongtai_conf/conf/uwsgi.ini  &
+  celery -A dongtai_conf beat -l info --pidfile= --scheduler django_celery_beat.schedulers:DatabaseScheduler
 else
 	# /usr/local/bin/uwsgi --ini /opt/dongtai/webapi/conf/uwsgi.ini --stats :3031 --stats-http
-	/usr/local/bin/uwsgi --ini /opt/dongtai/webapi/conf/uwsgi.ini
+	/usr/local/bin/uwsgi --ini /opt/dongtai/webapi/dongtai_conf/conf/uwsgi.ini
 fi
