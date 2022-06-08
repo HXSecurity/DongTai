39652e78106cecdcbdfb05425fa42b4eed79f256
zhaoyanwei <zhaoyanwei@huoxian.cn>
2022-06-08 19:24:16 +0800

    fix asset

--- dongtai_web/views/scas.py
+++ dongtai_web/views/scas.py
@@ -165,23 +165,10 @@ class ScaList(UserEndPoint):
 
         auth_user_ids = [str(_i.id) for _i in auth_users]
         auth_user_ids_str = ','.join(auth_user_ids)
-
-        base_query_sql = " LEFT JOIN iast_asset ON iast_asset.signature_value = iast_asset_aggr.signature_value WHERE iast_asset.user_id in ({}) and iast_asset.is_del=0 ".format(
+        base_query_sql = " LEFT JOIN iast_asset ON iast_asset.signature_value = iast_asset_aggr.signature_value WHERE iast_asset.dependency_level>0 and iast_asset.user_id in ({}) and iast_asset.is_del=0 ".format(
             auth_user_ids_str)
-        order_by = '-vul_count'
-        order = request.data.get('order', None)
-        if not order or order == "-":
-            order = '-vul_count'
-        else:
-            order_by = order
-        order_fields = [
-            'level', 'license', 'vul_count', 'project_count'
-        ]
-
-        order, order_type = get_order_params(order_fields, order)
-
-        order_sql = " order by {} {},iast_asset_aggr.id DESC  ".format(order, order_type)
-        page_sql = " limit {},{}".format(query_start, page_size)
+        list_sql_params = []
+        count_sql_params = []
 
         asset_aggr_where = " and iast_asset_aggr.id>0 "
 
@@ -194,20 +181,33 @@ class ScaList(UserEndPoint):
             else:
                 current_project_version = get_project_version_by_id(version_id)
 
-            base_query_sql = base_query_sql + " and iast_asset.project_id={} and iast_asset.project_version_id= {}".format(
-                project_id, current_project_version.get("version_id", 0))
+            base_query_sql = base_query_sql + " and iast_asset.project_id=%s and iast_asset.project_version_id=%s "
+            project_version_id = current_project_version.get("version_id", 0)
+            list_sql_params.append(project_id)
+            count_sql_params.append(project_id)
+            list_sql_params.append(project_version_id)
+            count_sql_params.append(project_version_id)
         total_count_sql = "SELECT count(distinct(iast_asset_aggr.id)) as alltotal FROM iast_asset_aggr {base_query_sql} {where_sql} limit 1 "
         list_query_sql = "SELECT iast_asset_aggr.id FROM iast_asset_aggr {base_query_sql} {where_sql} GROUP BY iast_asset_aggr.id {order_sql} {page_sql} "
 
         language = request_data.get('language', None)
         if language:
-            asset_aggr_where = asset_aggr_where + " and iast_asset_aggr.language in ({})".format(
-                "'" + "','".join(language) + "'")
+            language_str = "'" + "','".join(language) + "'"
+            asset_aggr_where = asset_aggr_where + " and iast_asset_aggr.language in ({})".format(language_str)
+        license_p = request_data.get('license', None)
+        if license_p:
+            if '未知' in license_p:
+                license_p.append("""""")
+
+            license_str = "'" + "','".join(license_p) + "'"
+            asset_aggr_where = asset_aggr_where + " and iast_asset_aggr.license in ({})".format(license_str)
 
         level_ids = request_data.get('level_id', None)
         if level_ids:
             level_ids = [str(x) for x in level_ids]
-            asset_aggr_where = asset_aggr_where + " and iast_asset_aggr.level_id in ({})".format(','.join(level_ids))
+            level_str = ','.join(level_ids)
+            level_str = pymysql.converters.escape_string(level_str)
+            asset_aggr_where = asset_aggr_where + " and iast_asset_aggr.level_id in ({})".format(level_str)
 
         package_kw = request_data.get('keyword', None)
         if package_kw:
@@ -215,6 +215,25 @@ class ScaList(UserEndPoint):
         if package_kw and package_kw.strip() != '':
             package_kw = '%%{}%%'.format(package_kw)
             asset_aggr_where = asset_aggr_where + " and iast_asset_aggr.package_name like %s "
+            list_sql_params.append(package_kw)
+            count_sql_params.append(package_kw)
+
+        order_by = '-vul_count'
+        order = request.data.get('order', None)
+        if not order or order == "-":
+            order = '-vul_count'
+        else:
+            order_by = order
+        order_fields = [
+            'level', 'license', 'vul_count', 'project_count'
+        ]
+
+        order, order_type = get_order_params(order_fields, order)
+
+        order_sql = " order by {} {},iast_asset_aggr.id DESC ".format(order, order_type)
+        page_sql = " limit %s,%s"
+        list_sql_params.append(query_start)
+        list_sql_params.append(page_size)
 
         total_count_sql = total_count_sql.format(base_query_sql=base_query_sql, where_sql=asset_aggr_where)
         list_query_sql = list_query_sql.format(base_query_sql=base_query_sql, where_sql=asset_aggr_where,
@@ -225,17 +244,13 @@ class ScaList(UserEndPoint):
         try:
             with connection.cursor() as cursor:
                 # 漏洞等级汇总
-                if package_kw:
-                    cursor.execute(total_count_sql, [package_kw])
-                else:
-                    cursor.execute(total_count_sql)
+                cursor.execute(total_count_sql, count_sql_params)
                 total_count_query = cursor.fetchone()
+
                 if total_count_query:
                     total_count = total_count_query[0]
-                if package_kw:
-                    cursor.execute(list_query_sql, [package_kw])
-                else:
-                    cursor.execute(list_query_sql)
+
+                cursor.execute(list_query_sql, list_sql_params)
                 page_data = cursor.fetchall()
                 if page_data:
                     sca_ids = [i[0] for i in page_data]
@@ -244,9 +259,9 @@ class ScaList(UserEndPoint):
 
         page_summary = {
             "alltotal": total_count,
-            "page_size": page_size,
-            "order": order,
-            "order_type": order_type
+            "page_size": page_size
+            # "order": order,
+            # "order_type": order_type
         }
         query_data = ScaAssetSerializer(
             AssetAggr.objects.filter(pk__in=sca_ids).order_by(order_by).select_related('level'),
