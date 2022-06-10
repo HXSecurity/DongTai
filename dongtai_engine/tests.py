924f0dd8dd447648befcba2fd0f01e6f58d0cf88
bidaya0 <xianzelin@huoxian.cn>
2022-06-10 12:01:59 +0800

    add-unit-test

--- dongtai_engine/tests.py
+++ dongtai_engine/tests.py
@@ -27,9 +27,8 @@ class CoreScanTestCase(AgentTestCase):
         cursor.execute(sqlfile)
 
     def test_benchmark_method_pool_scan(self):
-        data = MethodPool.objects.filter(agent_id=self.agent_id).all()
-        vul_count_begin = IastVulnerabilityModel.objects.filter(
-            agent_id=self.agent_id).all().count()
+        data = MethodPool.objects.all()
+        vul_count_begin = IastVulnerabilityModel.objects.all().count()
         for method_pool in data:
             method_pool.agent_id = self.agent_id
             method_pool.save()
@@ -37,15 +36,30 @@ class CoreScanTestCase(AgentTestCase):
                                         method_pool.agent_id)
             assert IastVulnerabilityModel.objects.filter(
                 url=method_pool.url, agent_id=self.agent_id).exists()
-        vul_count_after = IastVulnerabilityModel.objects.filter(
-            agent_id=self.agent_id).all().count()
+        vul_count_after = IastVulnerabilityModel.objects.all().count()
         assert len(data) == vul_count_after - vul_count_begin
 
+    def test_params_empty_count(self):
+        data = MethodPool.objects.all()
+        vul_count_without_param_mark_begin = IastVulnerabilityModel.objects.filter(
+            param_name='{}', level_id__lte=2).all().count()
+        for method_pool in data:
+            method_pool.agent_id = self.agent_id
+            method_pool.save()
+            search_vul_from_method_pool(method_pool.pool_sign,
+                                        method_pool.agent_id)
+            assert IastVulnerabilityModel.objects.filter(
+                url=method_pool.url, agent_id=self.agent_id).exists()
+        vul_count_without_param_mark_after = IastVulnerabilityModel.objects.filter(param_name='{}',
+            level_id__lte=2).all().count()
+        res = vul_count_without_param_mark_after - vul_count_without_param_mark_begin
+        print(res)
+        assert res == 262
 
 class CoreTaskTestCase(AgentTestCase):
 
     def test_search_method_pool(self):
-        method_pool_id = 4439061 
+        method_pool_id = 4439061
         method_pool = MethodPool.objects.filter(pk=method_pool_id).first()
         from dongtai_engine.tasks import search_vul_from_method_pool
         search_vul_from_method_pool(method_pool.pool_sign, method_pool.agent_id)
