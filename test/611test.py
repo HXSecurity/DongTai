f66259fdee0d2f3e8ad3494c8fe8f6cb52680225
jinghao <songjinghao2012@163.com>
2022-06-16 11:19:02 +0800

    rm some debug print

--- test/611test.py
+++
@@ -1,6 +0,0 @@
-import docker
-dockerclient = docker.DockerClient(base_url='unix:///var/run/docker.sock')
-for dp in dockerclient.containers.list():
-    print(dp)
-    print(db.images)
-    print(type(dp))
