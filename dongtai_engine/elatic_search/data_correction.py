2a58bc2d449a04698d088482c3e304393f4471c6
bidaya0 <xianzelin@huoxian.cn>
2022-06-30 16:08:51 +0800

    add-situation-when-project-missing

--- dongtai_engine/elatic_search/data_correction.py
+++ dongtai_engine/elatic_search/data_correction.py
@@ -1,7 +1,25 @@
 from dongtai_common.models.vulnerablity import IastVulnerabilityModel, IastVulnerabilityDocument
+from celery import shared_task
+from django.apps import apps
+from django.db import transaction
+from dongtai_common.models.asset import Asset, IastAssetDocument
+from dongtai_common.models.asset_vul import IastVulAssetRelation, IastAssetVulnerabilityDocument
+
+from celery.apps.worker import logger
+
+
+@shared_task
+def data_correction_interpetor(situation: str):
+    if situation == "project_missing":
+        data_correction(-1)
 
 
 def data_correction(project_id):
     qs = IastVulnerabilityModel.objects.filter(
         agent__bind_project_id=project_id).all()
     IastVulnerabilityDocument().update(list(qs))
+    qs = Asset.objects.filter(agent__bind_project_id=project_id).all()
+    IastAssetDocument().update(list(qs))
+    qs = IastVulAssetRelation.objects.filter(
+        asset__agent__bind_project_id=project_id).all()
+    IastAssetVulnerabilityDocument().update(list(qs))
