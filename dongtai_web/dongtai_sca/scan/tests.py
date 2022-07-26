0fbbbd69a72137b66793ba3ca0193c246aee2a72
bidaya0 <xianzelin@huoxian.cn>
2022-07-26 10:29:08 +0800

    add-save-asset_vul

--- dongtai_web/dongtai_sca/scan/tests.py
+++ dongtai_web/dongtai_sca/scan/tests.py
@@ -27,6 +27,65 @@ from test import DongTaiTestCase
 from test.apiserver.test_agent_base import AgentTestCase
 from .utils import update_one_sca
 
+from .utils import get_nearest_version
+
+class ExtenalApiTestCase(TestCase):
+
+    def test_nearest_version(self):
+        version_list = [
+            '1.0', '1.0-m4', '1.0-rc1', '1.0.1', '1.2', '1.2-rc1', '1.2-rc2',
+            '1.2.1', '1.2.2', '1.2.3', '1.2.4', '1.2.5', '1.2.6', '1.2.7',
+            '1.2.8', '1.2.9', '2.0', '2.0-m1', '2.0-m2', '2.0-m4', '2.0.1',
+            '2.0.2', '2.0.3', '2.0.4', '2.0.5', '2.0.6', '2.0.7', '2.0.8',
+            '2.5', '2.5.1', '2.5.2', '2.5.3', '2.5.4', '2.5.5', '2.5.6',
+            '2.5.6.SEC01', '2.5.6.SEC02', '2.5.6.SEC03', '3.0.0.RELEASE',
+            '3.0.1.RELEASE', '3.0.2.RELEASE', '3.0.3.RELEASE', '3.0.4.RELEASE',
+            '3.0.5.RELEASE', '3.0.6.RELEASE', '3.0.7.RELEASE', '3.1.0.RELEASE',
+            '3.1.1.RELEASE', '3.1.2.RELEASE', '3.1.3.RELEASE', '3.1.4.RELEASE',
+            '3.2.0.RELEASE', '3.2.1.RELEASE', '3.2.10.RELEASE',
+            '3.2.11.RELEASE', '3.2.12.RELEASE', '3.2.13.RELEASE',
+            '3.2.14.RELEASE', '3.2.15.RELEASE', '3.2.16.RELEASE',
+            '3.2.17.RELEASE', '3.2.18.RELEASE', '3.2.2.RELEASE',
+            '3.2.3.RELEASE', '3.2.4.RELEASE', '3.2.5.RELEASE', '3.2.6.RELEASE',
+            '3.2.7.RELEASE', '3.2.8.RELEASE', '3.2.9.RELEASE', '4.0.0.RELEASE',
+            '4.0.1.RELEASE', '4.0.2.RELEASE', '4.0.3.RELEASE', '4.0.4.RELEASE',
+            '4.0.5.RELEASE', '4.0.6.RELEASE', '4.0.7.RELEASE', '4.0.8.RELEASE',
+            '4.0.9.RELEASE', '4.1.0.RELEASE', '4.1.1.RELEASE', '4.1.2.RELEASE',
+            '4.1.3.RELEASE', '4.1.4.RELEASE', '4.1.5.RELEASE', '4.1.6.RELEASE',
+            '4.1.7.RELEASE', '4.1.8.RELEASE', '4.1.9.RELEASE', '4.2.0.RELEASE',
+            '4.2.1.RELEASE', '4.2.2.RELEASE', '4.2.3.RELEASE', '4.2.4.RELEASE',
+            '4.2.5.RELEASE', '4.2.6.RELEASE', '4.2.7.RELEASE', '4.2.8.RELEASE',
+            '4.2.9.RELEASE', '4.3.0.RELEASE', '4.3.1.RELEASE',
+            '4.3.10.RELEASE', '4.3.11.RELEASE', '4.3.12.RELEASE',
+            '4.3.13.RELEASE', '4.3.14.RELEASE', '4.3.15.RELEASE',
+            '4.3.16.RELEASE', '4.3.17.RELEASE', '4.3.18.RELEASE',
+            '4.3.19.RELEASE', '4.3.2.RELEASE', '4.3.20.RELEASE',
+            '4.3.21.RELEASE', '4.3.22.RELEASE', '4.3.23.RELEASE',
+            '4.3.24.RELEASE', '4.3.25.RELEASE', '4.3.26.RELEASE',
+            '4.3.27.RELEASE', '4.3.28.RELEASE', '4.3.29.RELEASE',
+            '4.3.3.RELEASE', '4.3.30.RELEASE', '4.3.4.RELEASE',
+            '4.3.5.RELEASE', '4.3.6.RELEASE', '4.3.7.RELEASE', '4.3.8.RELEASE',
+            '4.3.9.RELEASE', '5.0.0.RELEASE', '5.0.1.RELEASE',
+            '5.0.10.RELEASE', '5.0.11.RELEASE', '5.0.12.RELEASE',
+            '5.0.13.RELEASE', '5.0.14.RELEASE', '5.0.15.RELEASE',
+            '5.0.16.RELEASE', '5.0.17.RELEASE', '5.0.18.RELEASE',
+            '5.0.19.RELEASE', '5.0.2.RELEASE', '5.0.20.RELEASE',
+            '5.0.3.RELEASE', '5.0.4.RELEASE', '5.0.5.RELEASE', '5.0.6.RELEASE',
+            '5.0.7.RELEASE', '5.0.8.RELEASE', '5.0.9.RELEASE', '5.1.0.RELEASE',
+            '5.1.1.RELEASE', '5.1.10.RELEASE', '5.1.11.RELEASE',
+            '5.1.12.RELEASE', '5.1.13.RELEASE', '5.1.14.RELEASE',
+            '5.1.15.RELEASE', '5.1.16.RELEASE', '5.1.17.RELEASE',
+            '5.1.18.RELEASE', '5.1.19.RELEASE', '5.1.2.RELEASE',
+            '5.1.20.RELEASE', '5.1.3.RELEASE', '5.1.4.RELEASE',
+            '5.1.5.RELEASE', '5.1.6.RELEASE', '5.1.7.RELEASE', '5.1.8.RELEASE',
+            '5.1.9.RELEASE', '5.2.11.RELEASE', '5.2.19.RELEASE',
+            '5.2.3.RELEASE', '5.2.8.RELEASE', '5.3.14', '5.3.15', '5.3.16',
+            '5.3.19'
+        ]
+        version = '3.2.18.RELEASE'
+        nrversion = get_nearest_version(version, version_list)
+        assert nrversion == '4.0.0.RELEASE'
+
 
 class AgentHardencodeTestCase(AgentTestCase):
 
@@ -36,7 +95,7 @@ class AgentHardencodeTestCase(AgentTestCase):
             "/Users/xxx/spring-boot/2.3.2.RELEASE/org.springframework:spring-beans.jar",
             "a4bb5ffad5564e4a0e25955e3a40b1c6158385b2",
             "org.springframework:spring-beans.jar", "SHA-1")
-    
+
     def test_update_one_sca_golang(self):
         update_one_sca(
             self.agent_id,
--- dongtai_web/dongtai_sca/scan/utils.py
+++ dongtai_web/dongtai_sca/scan/utils.py
@@ -130,50 +130,135 @@ def update_one_sca(agent_id,
         asset.license = ''
         asset.dt = int(time.time())
         asset.save()
-        sca_scan_asset(asset.id, aql)
+        sca_scan_asset(asset.id, package['ecosystem'], package['name'],
+                       package['version'])
 
 from collections import defaultdict
 from dongtai_common.models.asset_vul import IastAssetVul
 
 
-def stat_severity(serveritys):
+def stat_severity(serveritys) -> defaultdict:
     dic = defaultdict(int)
     for serverity in serveritys:
-        if serverity not in dic:
-            dic[serverity] = 1
-        else:
-            dic[serverity] += 1
+        dic[serverity] += 1
     return dic
 
-#from dongtai_common.models.asset import Asset
-#def sca_scan_asset(asset_id: int, aql: str):
-#    package_vuls = get_package_vul(aql)
-#    res = stat_severity(map(lambda x: x["severity"], package_vuls))
-#    for vul in package_vuls:
-#        aql = get_package_aql(vul['name'], vul['ecosystem'], vul['version'])
-#        asset_vul = IastAssetVul.objects.create(
-#            package_name=vul['name'],
-#            level_id=vul_level,
-#            #license=vul_license, #???
-#            #license_level=vul_license_level, #???
-#            vul_name=vul['vul_title'],
-#            vul_detail=vul['description'],
-#            aql=aql,
-#            # package_hash=vul_package_hash, #???
-#            package_version=vul_package_v,
-#            package_safe_version=vul_package_safe_version,
-#            package_latest_version=vul_package_latest_version,
-#            package_language=vul_package_language,
-#            have_article=vul_have_article,
-#            have_poc=vul_have_poc,
-#            cve_id=cve_relation.id,
-#            cve_code=cve_relation.cve,
-#            vul_cve_nums=vul_reference,
-#            vul_serial=vul_serial,
-#            vul_publish_time=cve_relation.publish_time,
-#            vul_update_time=cve_relation.update_time,
-#            update_time=timestamp,
-#            update_time_desc=-timestamp,
-#            create_time=timestamp)
-#        pass
-#        #print(vul)
+from dongtai_common.models.asset import Asset
+from packaging.version import Version, parse
+
+
+def get_nearest_version(version_str: str, version_str_list: List[str]) -> str:
+    return min(filter(lambda x: x > parse(version_str), map(parse,
+                                                            version_str_list)))._version
+
+
+def get_latest_version(version_str_list: List[str]) -> str:
+    return max(map(parse, version_str_list))._version
+
+def get_cve_numbers(cve: Optional[str] = "",
+                    cwe: Optional[list] = [],
+                    cnvd: Optional[str] = "",
+                    cnnvd: Optional[str] = ""):
+    return {'cve': cve, 'cwe': cwe, 'cnvd': cnvd, 'cnnvd': cnnvd}
+
+
+def get_vul_serial(title: Optional[str] = "",
+                   cve: Optional[str] = "",
+                   cwe: Optional[list] = [],
+                   cnvd: Optional[str] = "",
+                   cnnvd: Optional[str] = "") -> str:
+    return "|".join([title, cve, cnvd, cnnvd] + cwe)
+
+
+from collections import defaultdict
+
+
+def get_vul_level_dict() -> defaultdict:
+    return defaultdict(lambda: 5, {
+        'high': 1,
+        "critical": 1,
+        "medium": 2,
+        "low": 3
+    })
+
+def get_ecosystem_language_dict() -> defaultdict:
+    return defaultdict(lambda: 'JAVA', {
+        'maven': 'JAVA',
+        "pypi": 'PYTHON',
+        "composer": 'PHP',
+        "golang": 'GO'
+    })
+
+def get_description(descriptions: List[Dict]) -> str:
+    if not descriptions:
+        return ""
+    return sorted(descriptions, key=lambda x: x['language'],
+                  reverse=True)[0]['content']
+
+
+def get_vul_path(base_aql: str,
+                 vul_package_path: List[Dict] = []) -> List[str]:
+    return list(
+        map(lambda x: get_package_aql(x['name'], x['ecosystem'], x['version']),
+            vul_package_path)) + [base_aql]
+
+
+from dongtai_common.models.asset_vul import IastAssetVul, IastVulAssetRelation, IastAssetVulType
+
+def sca_scan_asset(asset_id: int, ecosystem: str, package_name: str,
+                   version: str):
+    aql = get_package_aql(package_name, ecosystem, version)
+    package_vuls = get_package_vul(aql)
+    res = stat_severity(map(lambda x: x["severity"], package_vuls))
+    timestamp = int(time.time())
+    package_language = get_ecosystem_language_dict()[ecosystem]
+    Asset.objects.filter(pk=asset_id).update(
+        **{f"vul_{k}_count": v
+           for k, v in res.items()})
+    Asset.objects.filter(pk=asset_id).update(
+        **{"vul_count": sum(res.values())})
+    for vul in package_vuls:
+        vul_dependency = get_vul_path(aql, vul['vul_package_path'])
+        cve_numbers = get_cve_numbers(vul['cve'], vul['cwe_info'], vul['cnvd'],
+                                      vul['cnnvd'])
+        safe_version = get_nearest_version(version, vul['safe_version'])
+        vul_serial = get_vul_serial(vul['vul_title'], vul['cve'],
+                                    vul['cwe_info'], vul['cnvd'], vul['cnnvd'])
+        vul_level = get_vul_level_dict()[vul['severity']]
+
+        #still need?
+        #fix_version = get_nearest_version(version, vul['fixed'])
+        #latest_version = get_latest_version(vul['safe_version'])
+
+        # where to place?
+        #package_safe_version_list=vul['safe_version'],
+        #package_effected_version_list=vul['effected']
+        #package_fixed_version_list=vul['fixed']
+        asset_vul = IastAssetVul.objects.create(
+            package_name=vul['name'],
+            level_id=vul_level,
+            vul_name=vul['vul_title'],
+            vul_detail=vul['description'],
+            aql=aql,
+            # package_hash=vul_package_hash, #???
+            package_version=version,
+            package_safe_version=safe_version,
+            #package_latest_version=latest_version,
+            package_language=package_language,
+            have_article=1 if vul['references'] else 0,
+            have_poc=1 if vul['poc'] else 0,
+            #cve_id=cve_relation.id,
+            cve_code=vul['cve'],
+            vul_cve_nums=cve_numbers,
+            vul_serial=vul_serial,
+            vul_publish_time=vul['publish_time'],
+            vul_update_time=vul['vul_change_time'],
+            update_time=timestamp,
+            update_time_desc=-timestamp,
+            create_time=timestamp)
+        asset_vul_relation = IastVulAssetRelation.objects.create(
+            asset_vul_id=asset_vul.id,
+            asset_id=asset_id,
+            create_time=timestamp,
+            vul_dependency_path=vul_dependency,
+            status_id=1)
