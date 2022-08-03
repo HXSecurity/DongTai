1a31839254e1a29b90adbd10712480853d798b19
bidaya0 <xianzelin@huoxian.cn>
2022-08-03 16:13:34 +0800

    bugfix/asset_projects-and-package-vul

--- dongtai_web/dongtai_sca/views/asset_projects.py
+++ dongtai_web/dongtai_sca/views/asset_projects.py
@@ -31,21 +31,24 @@ class AssetProjects(UserEndPoint):
             auth_users = self.get_auth_users(request.user)
             asset_queryset = self.get_auth_assets(auth_users)
             asset = Asset.objects.filter(id=aggr_id).first()
-            asset_aggr = AssetAggr.objects.filter(
-                signature_value=asset.signature_value).first()
-            if not asset_aggr:
-                return R.failure(msg=_('Components do not exist or no permission to access'))
+            #asset_aggr = AssetAggr.objects.filter(
+            #    signature_value=asset.signature_value).first()
+            #if not asset_aggr:
+            #    return R.failure(msg=_('Components do not exist or no permission to access'))
 
-            asset_queryset = asset_queryset.filter(signature_value=asset_aggr.signature_value, dependency_level__gt=0,
-                                                   version=asset_aggr.version, project_id__gt=0).values('project_id',
-                                                                                                        'id').all()
+            asset_queryset = asset_queryset.filter(
+                signature_value=asset.signature_value,
+                version=asset.version,
+                project_id__gt=0).values('project_id', 'id').all()
             if not asset_queryset:
-                return R.failure(msg=_('Components do not exist or no permission to access'))
+                return R.failure(msg=_(
+                    'Components do not exist or no permission to access'))
 
             _temp_data = {_a['project_id']: _a['id'] for _a in asset_queryset}
             asset_ids = [_temp_data[p_id] for p_id in _temp_data]
 
-            data = AssetProjectSerializer(Asset.objects.filter(pk__in=asset_ids), many=True).data
+            data = AssetProjectSerializer(
+                Asset.objects.filter(pk__in=asset_ids), many=True).data
 
             return R.success(data=data)
         except Exception as e:
