2c91500fb30ac40590996e761f76aca1dbbec179
bidaya0 <xianzelin@huoxian.cn>
2022-08-16 17:29:54 +0800

    pep8fix: E222

--- deploy/commands/management/commands/statistics.py
+++ deploy/commands/management/commands/statistics.py
@@ -14,20 +14,30 @@ class Command(BaseCommand):
         #parser.add_argument('update', nargs='+', type=int)
 
     def handle(self, *args, **options):
-        agent_count =  IastAgent.objects.all().count()
+        agent_count = IastAgent.objects.all().count()
         online_agent_count = IastAgent.objects.filter(online=1).count()
         total_method_pool_count = MethodPool.objects.all().count()
         total_vulnerability = IastVulnerabilityModel.objects.all().count()
-        total_high_vulnerability = IastVulnerabilityModel.objects.filter(level_id=1).count()
-        total_medium_vulnerability = IastVulnerabilityModel.objects.filter(level_id=2).count()
-        total_low_vulnerability = IastVulnerabilityModel.objects.filter(level_id=3).count()
-        total_info_vulnerability = IastVulnerabilityModel.objects.filter(level_id=4).count()
-        total_note_vulnerability = IastVulnerabilityModel.objects.filter(level_id=5).count()
-        total_high_asset_vulnerability =  IastAssetVul.objects.filter(level_id=1).count()
-        total_medium_asset_vulnerability = IastAssetVul.objects.filter(level_id=2).count()
-        total_low_asset_vulnerability = IastAssetVul.objects.filter(level_id=3).count()
-        total_info_asset_vulnerability = IastAssetVul.objects.filter(level_id=4).count()
-        total_note_asset_vulnerability = IastAssetVul.objects.filter(level_id=5).count()
+        total_high_vulnerability = IastVulnerabilityModel.objects.filter(
+            level_id=1).count()
+        total_medium_vulnerability = IastVulnerabilityModel.objects.filter(
+            level_id=2).count()
+        total_low_vulnerability = IastVulnerabilityModel.objects.filter(
+            level_id=3).count()
+        total_info_vulnerability = IastVulnerabilityModel.objects.filter(
+            level_id=4).count()
+        total_note_vulnerability = IastVulnerabilityModel.objects.filter(
+            level_id=5).count()
+        total_high_asset_vulnerability = IastAssetVul.objects.filter(
+            level_id=1).count()
+        total_medium_asset_vulnerability = IastAssetVul.objects.filter(
+            level_id=2).count()
+        total_low_asset_vulnerability = IastAssetVul.objects.filter(
+            level_id=3).count()
+        total_info_asset_vulnerability = IastAssetVul.objects.filter(
+            level_id=4).count()
+        total_note_asset_vulnerability = IastAssetVul.objects.filter(
+            level_id=5).count()
 
         self.stdout.write(f"""
         =============================================
