bec218b479c495d433f6997aedb99d2b76b9cc9a
tscuite <tscuite@qq.com>
2022-09-09 19:28:27 +0800

    fix: docker

--- deploy/latest_image.sh
+++ deploy/latest_image.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 images=("dongtai-server" "dongtai-web" "dongtai-mysql" "dongtai-redis" "dongtai-logrotate" "dongtai-logstash")
 for image in ${images[*]}; do
-     tags=`wget -q https://registry.hub.docker.com/v1/repositories/dongtai/${image}/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | grep -E '^([0-9]+\.){0,2}(\*|[0-9]+)$' | tail -n 1 `
+     tags=`curl -s -S "https://registry.hub.docker.com/v2/repositories/dongtai/${image}/tags" | sed -e 's/,/,\n/g' -e 's/\[/\[\n/g' | grep '"name"' | awk -F\" '{print $4;}' | sort -rfu | sed -n '2p'`
      echo "$image"
+     echo "$tags"
      echo -e "\t dongtai/$image:$tags"
      echo -e "\t registry.cn-beijing.aliyuncs.com/huoxian_pub/$image:$tags"
 done
