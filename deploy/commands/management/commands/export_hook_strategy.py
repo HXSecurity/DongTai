2625fefc2da119cdcbe740b71f59571a2c23dae6
bidaya0 <xianzelin@huoxian.cn>
2022-10-21 16:56:02 +0800

    feat(strategy):  add load hook strategy command .

--- deploy/commands/management/commands/export_hook_strategy.py
+++ deploy/commands/management/commands/export_hook_strategy.py
@@ -5,10 +5,45 @@ import json
 from dongtai_conf.settings import BASE_DIR
 import os
 from dongtai_common.models.strategy import IastStrategyModel
+from dongtai_common.models.sensitive_info import IastSensitiveInfoRule
+from dongtai_common.models.hook_strategy import HookStrategy
+from dongtai_common.models.hook_type import HookType
+from django.forms.models import model_to_dict
 
 
-def export_strategy():
-    pass
+def export_strategy() -> list:
+    qs1 = IastStrategyModel.objects.filter(
+        strategy__id__isnull=False).values_list('id', flat=True)
+    qs2 = IastStrategyModel.objects.filter(
+        iastsensitiveinforule__id__isnull=False).values_list('id', flat=True)
+    strategies = IastStrategyModel.objects.filter(
+        pk__in=list(qs1.union(qs2))).order_by('id').all()
+
+    strategies_res = [
+        model_to_dict(i,
+                      exclude=[
+                          'id', 'hook_type', 'vul_strategy', 'create_time',
+                          'update_time', 'dt'
+                      ]) for i in strategies
+    ]
+    return strategies_res
+
+
+def export_hooktype(language_id: int) -> list:
+    qs1 = HookType.objects.filter(language_id=language_id,
+                                  strategy__id__isnull=False).values_list(
+                                      'id', flat=True)
+    strategies = HookType.objects.filter(language_id=language_id,
+                                         pk__in=list(qs1)).order_by('id').all()
+
+    strategies_res = [
+        model_to_dict(i,
+                      exclude=[
+                          'id', 'hook_type', 'vul_strategy', 'create_time',
+                          'update_time', 'dt'
+                      ]) for i in strategies
+    ]
+    return strategies_res
 
 class Command(BaseCommand):
     help = 'export all hook_strategy'
@@ -33,6 +68,12 @@ class Command(BaseCommand):
                     os.path.join(POLICY_DIR, f'{k.lower()}_full_policy.json'),
                     'w+') as fp:
                 json.dump(c, fp, indent=4)
+            with open(
+                    os.path.join(POLICY_DIR, f'{k.lower()}_hooktype.json'),
+                    'w+') as fp:
+                json.dump(export_hooktype(language_id=v), fp, indent=4)
+        with open(os.path.join(POLICY_DIR, f'vul_strategy.json'), 'w+') as fp:
+            json.dump(export_strategy(), fp, indent=4)
 
         self.stdout.write(
             self.style.SUCCESS('Successfully export strategy .'))
---
+++ deploy/commands/management/commands/load_hook_strategy.py
@@ -0,0 +1,121 @@
+from django.core.management.base import BaseCommand
+from dongtai_common.models.agent_method_pool import MethodPool
+from dongtai_protocol.views.hook_profiles import HookProfilesEndPoint, LANGUAGE_DICT
+import json
+from dongtai_conf.settings import BASE_DIR
+import os
+from dongtai_common.models.strategy import IastStrategyModel
+from dongtai_common.models.sensitive_info import IastSensitiveInfoRule
+from dongtai_common.models.hook_strategy import HookStrategy
+from dongtai_common.models.hook_type import HookType
+from django.forms.models import model_to_dict
+
+
+class Command(BaseCommand):
+    help = 'load hook_strategy'
+    functions = []
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        POLICY_DIR = os.path.join(BASE_DIR, 'static/data/')
+        with open(os.path.join(POLICY_DIR, f'vul_strategy.json')) as fp:
+            full_strategies = json.load(fp)
+        strategy_dict = {}
+        for strategy in full_strategies:
+            if IastStrategyModel.objects.filter(
+                    vul_type=strategy['vul_type'],
+                    strategy__id__isnull=False,
+                    system_type=1,
+            ).exists() or IastStrategyModel.objects.filter(
+                    vul_type=strategy['vul_type'],
+                    iastsensitiveinforule__id__isnull=False,
+                    system_type=1).exists():
+                #已存在策略类型，不会重建
+                qs1 = IastStrategyModel.objects.filter(
+                    vul_type=strategy['vul_type'],
+                    system_type=1,
+                    strategy__id__isnull=False).values_list('id', flat=True)
+                qs2 = IastStrategyModel.objects.filter(
+                    vul_type=strategy['vul_type'],
+                    system_type=1,
+                    iastsensitiveinforule__id__isnull=False).values_list(
+                        'id', flat=True)
+                strategy_obj = IastStrategyModel.objects.filter(
+                    pk__in=list(qs1.union(qs2))).order_by('id').first()
+                # strategy_obj = IastStrategyModel.objects.filter(vul_type=strategy['vul_type'],system_type=1).first()
+                strategy_dict[strategy['vul_type']] = strategy_obj
+                continue
+            if IastStrategyModel.objects.filter(
+                    vul_type=strategy['vul_type'],
+                    strategy__id__isnull=False,
+                    system_type=0).exists(
+                    ) or IastStrategyModel.objects.filter(
+                        vul_type=strategy['vul_type'],
+                        iastsensitiveinforule__id__isnull=False,
+                        system_type=0).exists():
+                #存在用户定义的冲突策略,不会修改
+                continue
+            #strategy['language_id'] = strategy['language']
+            #del strategy['language']
+            strategy['user_id'] = strategy['user']
+            del strategy['user']
+            strategy['level_id'] = strategy['level']
+            del strategy['level']
+            print(strategy)     
+            strategy_obj = IastStrategyModel.objects.create(**strategy)
+            strategy_dict[strategy['vul_type']] = strategy_obj
+        for k, v in LANGUAGE_DICT.items():
+            with open(os.path.join(POLICY_DIR,
+                                   f'{k.lower()}_hooktype.json')) as fp:
+                hooktypes = json.load(fp)
+            hooktype_dict = {}
+            for hook_type in hooktypes:
+                if HookType.objects.filter(value=hook_type['value'],
+                                           strategy__id__isnull=False,
+                                           system_type=1).exists():
+                    #已存在策略类型，不会重建,会将新的规则添加到这上边
+                    hooktype_obj = HookType.objects.filter(
+                        value=hook_type['value'],
+                        strategy__id__isnull=False,
+                        system_type=1).first()
+                    hooktype_dict[hook_type['value']] = hooktype_obj
+                    continue
+                if HookType.objects.filter(value=hook_type['value'],
+                                           strategy__id__isnull=False,
+                                           system_type=0).exists():
+                    #存在用户定义的冲突策略,不会修改
+                    continue
+                #del hook_type['created_by']
+                del hook_type['language']
+                hook_type['language_id'] = v
+                hooktype_obj = HookType(**hook_type)
+                hooktype_obj.save()
+                hooktype_dict[hook_type['value']] = hooktype_obj
+
+            with open(os.path.join(POLICY_DIR,
+                                   f'{k.lower()}_full_policy.json')) as fp:
+                full_policy = json.load(fp)
+            for policy in full_policy:
+                if 'type' not in policy.keys():
+                    policy['type'] = 4
+                if policy['type'] == 4:
+                    if policy['vul_type'] not in strategy_dict.keys():
+                        continue
+                    policy_strategy = strategy_dict[policy['vul_type']]
+                    for hook_strategy in policy['details']:
+                        del hook_strategy['language']
+                        hook_strategy['language_id'] = v
+                        HookStrategy.objects.create(strategy=policy_strategy,
+                                                    **hook_strategy)
+                else:
+                    if policy['value'] not in hooktype_dict.keys():
+                        continue
+                    policy_hook_type = hooktype_dict[policy['value']]
+                    for hook_strategy in policy['details']:
+                        del hook_strategy['language']
+                        hook_strategy['language_id'] = v
+                        HookStrategy.objects.create(hooktype=policy_hook_type,
+                                                    **hook_strategy)
+        self.stdout.write(self.style.SUCCESS('Successfully load strategy .'))
