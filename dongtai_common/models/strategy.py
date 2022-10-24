1a5f766ccf421d4fd3cd68653e5cdad89267e99e
bidaya0 <xianzelin@huoxian.cn>
2022-10-24 16:45:53 +0800

    feat(strategy):  add strategy time default value .

--- dongtai_common/models/strategy.py
+++ dongtai_common/models/strategy.py
@@ -4,14 +4,14 @@ from dongtai_common.models import User
 from dongtai_common.models.hook_type import HookType
 from dongtai_common.models.vul_level import IastVulLevel
 from dongtai_common.utils.settings import get_managed
-
+from time import time
 
 class IastStrategyModel(models.Model):
     user = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True)
     vul_type = models.CharField(max_length=255, blank=True, null=True)
     level = models.ForeignKey(IastVulLevel, models.DO_NOTHING, blank=True, null=True)
     state = models.CharField(max_length=255, blank=True, null=True)
-    dt = models.IntegerField(blank=True, null=True)
+    dt = models.IntegerField(blank=True, null=True, default=time)
     vul_name = models.CharField(max_length=255, blank=True, null=True)
     vul_desc = models.TextField(blank=True, null=True)
     vul_fix = models.TextField(blank=True, null=True)
