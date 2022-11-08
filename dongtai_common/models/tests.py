50b966762239893278937cd3a39007d057c24527
bidaya0 <xianzelin@huoxian.cn>
2022-11-08 15:18:58 +0800

    feat(basemodel): add base test .

--- dongtai_common/models/tests.py
+++ dongtai_common/models/tests.py
@@ -16,3 +16,22 @@ class VulEngineSearchTestCase(TestCase):
                               value='123123',
                               user_id=1,
                               state=State.ENABLE)
+        rule = IastAgentBlackRule.objects.filter(user_id=1).first()
+        assert [{
+            'target_type': 'URL',
+            'operator': 'CONTAIN',
+            'value': '123123'
+        }] == rule.to_full_rule()
+    
+    def test_url_rule_create_header(self):
+        create_blacklist_rule(TargetType.HEADER_KEY,
+                              TargetOperator.CONTAIN,
+                              value='123123',
+                              user_id=1,
+                              state=State.ENABLE)
+        rule = IastAgentBlackRule.objects.filter(user_id=1).first()
+        assert [{
+            'target_type': 'HEADER_KEY',
+            'operator': 'CONTAIN',
+            'value': '123123'
+        }] == rule.to_full_rule()
