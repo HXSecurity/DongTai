c55fd8b8d4052370f1729c400f83a10d9ade38cf
bidaya0 <xianzelin@huoxian.cn>
2022-11-10 14:32:44 +0800

    feat(method pool): add compatibility to display and new highlight range.

--- dongtai_common/engine/compatibility.py
+++ dongtai_common/engine/compatibility.py
@@ -41,8 +41,34 @@ def method_pool_3_to_2(dic: dict) -> dict:
     dic['targetValues'] = ','.join(targetValues)
     return dic
 
-def parse_target_value(target_value: str):
+def parse_target_value(target_value: str) -> str:
+    if not target_value:
+        return target_value
     position = target_value.rfind('*')
     origin_str = target_value[0:position][1:-1]
-    #len_of_origin = int(target_value[position::])
     return origin_str
+
+
+def parse_target_value_length(target_value: str) -> int:
+    if not target_value:
+        return 0
+    position = target_value.rfind('*')
+    len_of_origin = int(target_value[position+1::])
+    return len_of_origin
+
+from typing import List
+
+
+def highlight_target_value(target_value: str, ranges: List) -> str:
+    value = parse_target_value(target_value)
+    value_origin_len = parse_target_value_length(target_value)
+    if not value:
+        return target_value
+    if ranges and value and len(value) == value_origin_len:
+        ranges = sorted(ranges, key = lambda x: x['start'])
+        final_str = []
+        for range_ in ranges:
+            final_str.append(
+                f"<emt>{value[range_['start']:range_['stop']]}</emt>")
+        return "".join(final_str)
+    return f"<emt>{value}</emt>"
