1e2ebf9fc7c904197fb208b047c7180f554f8bda
bidaya0 <xianzelin@huoxian.cn>
2022-11-14 17:02:01 +0800

    dev(cython): add setup.py.

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 import glob
+import os
 
-dir_set = set(glob.glob('**/*.py',recursive=True))
+dir_set = set(glob.glob('**/*.py', recursive=True))
 
 dir_set.remove('manage.py')
 dir_set.remove('setup.py')
 
-dir_set = set(filter(lambda x: 'deploy/commands' not in x,dir_set))
+dir_set = set(filter(lambda x: 'deploy/commands' not in x, dir_set))
+ntheards = os.cpu_count()
 
-setup(
-   ext_modules=cythonize(dir_set,compiler_directives={'language_level': 3},exclude_failures=True)
-)
+setup(ext_modules=cythonize(dir_set,
+                            compiler_directives={'language_level': 3},
+                            exclude_failures=True,
+                            ntheards=ntheards))
