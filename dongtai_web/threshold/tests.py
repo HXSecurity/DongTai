87a60744cc8a6c08b8ffeba791840022a02f82a2
bidaya0 <xianzelin@huoxian.cn>
2022-12-21 14:47:25 +0800

    fix: add test case.

--- dongtai_web/threshold/tests.py
+++ dongtai_web/threshold/tests.py
@@ -9,9 +9,10 @@ class TypingTestCase(TestCase):
 
     def test_typing_in_get_data_from_dict_by_key(self, ):
         get_data_from_dict_by_key({"213123132": "123123132"}, ("213123132", ))
-        get_data_from_dict_by_key(
-            AgentConfigSettingV2TargetSerializer(data={
-                "target_type": 1,
-                "opt": 1,
-                "value": "21313"
-            }).data, ("target_type", "opt", "value"))
+        ser = AgentConfigSettingV2TargetSerializer(data={
+            "target_type": 1,
+            "opt": 1,
+            "value": "21313"
+        })
+        ser.is_valid()
+        get_data_from_dict_by_key(ser.data, ("target_type", "opt", "value"))
