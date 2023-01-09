dd4ccb39012ede572834b344cb7f867277a16efc
bidaya0 <xianzelin@huoxian.cn>
2023-01-09 17:22:12 +0800

    fix(scan): fix engine scan in branch.

--- dongtai_common/engine/vul_engine.py
+++ dongtai_common/engine/vul_engine.py
@@ -169,11 +169,11 @@ class VulEngine(object):
                                     source_type_hash]['ranges']:
                                 the_second_stack = stack
                                 break
+            if not the_second_stack:
+                return
             self.vul_source_signature = None
             self.vul_stack = []
             self.pool_value = set(the_second_stack.get('sourceHash'))
-            if not the_second_stack:
-                return
             for ind, method in enumerate(self.method_pool):
                 if method['invokeId'] == the_second_stack['invokeId']:
                     index = ind
