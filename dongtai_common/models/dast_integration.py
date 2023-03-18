135a894bc15369f196d1d5214a2cae8428824b13
bidaya0 <xianzelin@huoxian.cn>
2023-03-18 18:09:32 +0800

    feat: integrate with dast.

--- dongtai_common/models/dast_integration.py
+++ dongtai_common/models/dast_integration.py
@@ -1,17 +1,50 @@
 from dongtai_common.models.vulnerablity import IastVulnerabilityModel
 from django.db import models
 from dongtai_common.utils.settings import get_managed
+from dongtai_common.models.project import IastProject
+from dongtai_common.models.project_version import IastProjectVersion
+from dongtai_common.models.vul_level import IastVulLevel
+import time
 
 
 class IastDastIntegration(models.Model):
-    vul = models.ForeignKey(
-        IastVulnerabilityModel,
-        models.DO_NOTHING,
+    vul_name = models.CharField(max_length=255, blank=True, null=True)
+    detail = models.TextField(
         blank=True,
         null=True,
+        default='',
     )
-    uuid = models.CharField(max_length=255, blank=True, null=True)
-    data = models.JSONField(null=False, default={})
+    vul_level = models.ForeignKey(IastVulLevel,
+                                  models.DO_NOTHING,
+                                  blank=True,
+                                  null=True)
+    payload = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        default='',
+    )
+    target = models.CharField(max_length=255, blank=True, null=True)
+    vul_type = models.CharField(max_length=255, blank=True, null=True)
+    dast_tag = models.CharField(max_length=255, blank=True, null=True)
+    request_messages = models.JSONField(null=False, default=[])
+    urls = models.JSONField(null=False, default=[])
+    create_time = models.IntegerField(default=lambda: int(time.time()),
+                                      blank=True,
+                                      null=True)
+    latest_time = models.IntegerField(default=lambda: int(time.time()),
+                                      blank=True,
+                                      null=True)
+    project = models.ForeignKey(IastProject,
+                                on_delete=models.CASCADE,
+                                blank=True,
+                                null=True,
+                                default=-1)
+    project_version = models.ForeignKey(IastProjectVersion,
+                                        on_delete=models.CASCADE,
+                                        blank=True,
+                                        null=True,
+                                        default=-1)
 
     class Meta:
         managed = get_managed()
