2055fa7bfd90f0177640f7d2ed96137886266132
bidaya0 <xianzelin@huoxian.cn>
2023-03-29 17:51:08 +0800

    fix: ssrf fix and add testcase.

--- dongtai_common/engine/vul_engine.py
+++ dongtai_common/engine/vul_engine.py
@@ -169,10 +169,10 @@ class VulEngine(object):
                                     source_type_hash]['ranges']:
                                 the_second_stack = stack
                                 break
-            if not the_second_stack:
-                return
             self.vul_source_signature = None
             self.vul_stack = []
+            if not the_second_stack:
+                return
             self.pool_value = set(the_second_stack.get('sourceHash'))
             for ind, method in enumerate(self.method_pool):
                 if method['invokeId'] == the_second_stack['invokeId']:
