fdc3b0f3229c39870254e765ab009edc3aa43b5b
bidaya0 <xianzelin@huoxian.cn>
2023-05-24 17:43:11 +0800

    feat: optimise agent upload.

--- dongtai_conf/wsgi.py
+++ dongtai_conf/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+ENABLE_GEVENT = os.environ.get("GEVENT", 'FALSE') == 'TRUE'
+if ENABLE_GEVENT:
+    from gevent import monkey
+    monkey.patch_all()
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dongtai_conf.settings')
