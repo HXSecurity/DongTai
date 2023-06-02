1bbec0f22474fe67c285d80dfaf5edc79e28a5ba
bidaya0 <xianzelin@huoxian.cn>
2023-06-02 16:33:39 +0800

    feat: new sca impl api add field.

--- dongtai_common/models/asset_vul_v2.py
+++ dongtai_common/models/asset_vul_v2.py
@@ -31,6 +31,8 @@ class IastAssetVulV2(models.Model):
                               unique=True)
     vul_type = models.JSONField(blank=True, null=True)
     vul_codes = models.JSONField(blank=True, null=True)
+    affected_versions= models.JSONField(blank=True, null=True)
+    unaffected_versions = models.JSONField(blank=True, null=True)
 
     class Meta:
         managed = True
