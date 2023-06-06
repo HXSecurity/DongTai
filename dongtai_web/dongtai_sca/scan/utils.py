0b0bc6c17b1389e3728b63a251ed1ec0228b4bfe
bidaya0 <xianzelin@huoxian.cn>
2023-06-06 11:33:31 +0800

    fix: type infer in license.

--- dongtai_web/dongtai_sca/scan/utils.py
+++ dongtai_web/dongtai_sca/scan/utils.py
@@ -922,9 +922,10 @@ def get_license_list(license_list_str: str) -> List[Dict]:
 
 def get_license_list_v2(license_list: Tuple[str]) -> List[Dict]:
     filter_none: Callable[[Optional[Dict]], bool] = lambda x: x is not None
-    res = list(
-        filter(filter_none,
-               map(lambda x: LICENSE_DICT.get(x, None), license_list)))
+    res = [
+        LICENSE_DICT[license] for license in license_list
+        if license in LICENSE_DICT
+    ]
     return res
     #return [{
     #    'identifier': "无",
