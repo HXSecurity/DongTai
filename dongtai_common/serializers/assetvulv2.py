6616fc625641719ca78e84932c635ac52aaa6458
bidaya0 <xianzelin@huoxian.cn>
2023-06-08 16:08:28 +0800

    fix: new asset risk level.

--- dongtai_common/serializers/assetvulv2.py
+++ dongtai_common/serializers/assetvulv2.py
@@ -1,10 +1,9 @@
-
 from rest_framework import serializers
 from dongtai_common.models.asset_vul_v2 import IastAssetVulV2
+
 
 class PackageVulSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = IastAssetVulV2
         fields = '__all__'
-
