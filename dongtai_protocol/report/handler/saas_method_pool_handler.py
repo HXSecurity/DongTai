3a1b83f81f2b96bd958a034efc579d8579e14fa2
bidaya0 <xianzelin@huoxian.cn>
2023-06-27 15:17:13 +0800

    fix:  remove not reliable api data.

--- dongtai_protocol/report/handler/saas_method_pool_handler.py
+++ dongtai_protocol/report/handler/saas_method_pool_handler.py
@@ -104,8 +104,6 @@ class SaasMethodPoolHandler(IReportHandler):
         :return:
         """
         headers = SaasMethodPoolHandler.parse_headers(self.http_req_header)
-        #save_project_header(list(headers.keys()), self.agent_id)
-        add_new_api_route(self.agent, self.http_uri, self.http_method)
         import base64
         params_dict = get_params_dict(base64.b64decode(self.http_req_header),
                                       self.http_req_data,
