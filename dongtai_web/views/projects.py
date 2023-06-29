353f221556796b9bf0eb87c683bca2821af8bc98
st1020 <stone_1020@qq.com>
2023-06-29 16:51:40 +0800

    feat: add project stauts filter

--- dongtai_web/views/projects.py
+++ dongtai_web/views/projects.py
@@ -6,7 +6,6 @@
 
 import logging
 
-from dongtai_common.models.agent import IastAgent
 from dongtai_common.endpoint import R
 from dongtai_common.endpoint import UserEndPoint
 from dongtai_common.models.project import IastProject
@@ -25,13 +24,19 @@ logger = logging.getLogger("django")
 
 
 class _ProjectsArgsSerializer(serializers.Serializer):
-    pageSize = serializers.IntegerField(default=20,
-                                        help_text=_('Number per page'))
-    page = serializers.IntegerField(min_value=1, default=1, help_text=_('Page index'))
+    pageSize = serializers.IntegerField(default=20, help_text=_("Number per page"))
+    page = serializers.IntegerField(min_value=1, default=1, help_text=_("Page index"))
     name = serializers.CharField(
         default=None,
-        help_text=_(
-            "The name of the item to be searched, supports fuzzy search."))
+        help_text=_("The name of the item to be searched, supports fuzzy search."),
+    )
+    status = serializers.IntegerField(
+        default=None,
+        allow_null=True,
+        min_value=0,
+        max_value=10,
+        help_text=_("The project status."),
+    )
 
 
 _SuccessSerializer = get_response_serializer(ProjectSerializer(many=True))
@@ -43,28 +48,35 @@ class Projects(UserEndPoint):
 
     @extend_schema_with_envcheck(
         [_ProjectsArgsSerializer],
-        tags=[_('Project')],
-        summary=_('Projects List'),
-        description=_("Get the item corresponding to the user, support fuzzy search based on name."
-                      ),
+        tags=[_("Project")],
+        summary=_("Projects List"),
+        description=_(
+            "Get the item corresponding to the user, support fuzzy search based on name."
+        ),
         response_schema=_SuccessSerializer,
     )
     def get(self, request):
         ser = _ProjectsArgsSerializer(data=request.GET)
         try:
             if ser.is_valid(True):
-                page = ser.validated_data.get('page', 1)
-                page_size = ser.validated_data.get('pageSize', 20)
-                name = ser.validated_data.get('name')
+                page: int = ser.validated_data.get("page", 1)
+                page_size: int = ser.validated_data.get("pageSize", 20)
+                name: str = ser.validated_data.get("name")
+                status: int | None = ser.validated_data.get("status")
+            else:
+                return R.failure(data="Can not validation data.")
         except ValidationError as e:
             return R.failure(data=e.detail)
 
         # users = self.get_auth_users(request.user)
         department = request.user.get_relative_department()
-        queryset = IastProject.objects.filter(
-            department__in=department).order_by('-latest_time')
+        queryset = IastProject.objects.filter(department__in=department).order_by(
+            "-latest_time"
+        )
         if name:
             queryset = queryset.filter(name__icontains=name)
+        if status is not None:
+            queryset = queryset.filter(status=status)
         page_summary, page_data = self.get_paginator(queryset, page, page_size)
         vul_levels_dict = get_vul_levels_dict(page_data)
         project_language_dict = get_project_language(page_data)
@@ -74,9 +86,9 @@ class Projects(UserEndPoint):
                 page_data,
                 many=True,
                 context={
-                    'vul_levels_dict': vul_levels_dict,
-                    'project_language_dict': project_language_dict,
-                    'agent_count_dict': agent_count_dict,
+                    "vul_levels_dict": vul_levels_dict,
+                    "project_language_dict": project_language_dict,
+                    "agent_count_dict": agent_count_dict,
                 },
             ).data,
             page=page_summary,
