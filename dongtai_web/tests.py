d8fbb655ed8cb083e243256fc8e88cc923ac4087
st1020 <stone_1020@qq.com>
2023-07-07 15:40:00 +0800

    fix: fix test error

--- dongtai_web/tests.py
+++ dongtai_web/tests.py
@@ -1,9 +1,7 @@
 # Create your tests here.
 from django.test import TestCase
 from dongtai_web.views.agents_v2 import query_agent
-from dongtai_web.views.log_download import (file_newest_N_file_under_path, getzipfilesinmemorty, )
-from dongtai_web.views.agents_v2 import (
-    query_agent, )
+from dongtai_web.views.agents_v2 import query_agent
 
 from dongtai_web.threshold.config_setting import (convert_choices_to_dict,
                                                   convert_choices_to_value_dict,
@@ -24,22 +22,6 @@ from dongtai_common.models.agent_config import (
 class DashboardTestCase(TestCase):
     def test_query_agent(self):
         res = query_agent()
-        print(res)
-
-
-class ZipFileTestCase(TestCase):
-
-    def test_findnewest_file(self):
-        res = file_newest_N_file_under_path('./dongtai_web', 2)
-        print(res)
-
-    def test_getzipfilesinmemorty(self):
-        res = getzipfilesinmemorty(['./README.md', 'Pipfile'])
-        print(res)
-
-    def test_get_zip_together(self):
-        from dongtai_web.views.log_download import get_zip_together
-        res = get_zip_together([1, 2, 3], 1)
         print(res)
 
 
