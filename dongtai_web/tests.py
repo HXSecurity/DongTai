f4da1457e2894258d60a4fdaead7c22c887a4bcf
st1020 <stone_1020@qq.com>
2023-07-07 15:42:47 +0800

    fix: fix test error

--- dongtai_web/tests.py
+++ dongtai_web/tests.py
@@ -1,7 +1,6 @@
 # Create your tests here.
 from django.test import TestCase
 from dongtai_web.views.agents_v2 import query_agent
-from dongtai_web.views.agents_v2 import query_agent
 
 from dongtai_web.threshold.config_setting import (convert_choices_to_dict,
                                                   convert_choices_to_value_dict,
