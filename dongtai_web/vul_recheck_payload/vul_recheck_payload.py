3f9d3b4553cf86f528ba1a7e397e98c75d8126f6
st1020 <gaoyishi@huoxian.cn>
2023-07-17 14:12:12 +0800

    fix: fix IastVulRecheckPayloadSerializer error

--- dongtai_web/vul_recheck_payload/vul_recheck_payload.py
+++ dongtai_web/vul_recheck_payload/vul_recheck_payload.py
@@ -1,16 +1,10 @@
-import time
-
-from dongtai_common.endpoint import UserEndPoint, R, TalentAdminEndPoint
-from dongtai_common.models.agent_config import IastAgentConfig
+from dongtai_common.endpoint import UserEndPoint, R
 from django.utils.translation import gettext_lazy as _
-from dongtai_web.utils import extend_schema_with_envcheck, get_response_serializer
-from dongtai_web.serializers.agent_config import AgentConfigSettingSerializer
 from rest_framework.serializers import ValidationError
 from rest_framework import viewsets
 from dongtai_common.models.vul_recheck_payload import IastVulRecheckPayload
 from rest_framework import serializers
 from django.db.models import Q
-from django.forms.models import model_to_dict
 from drf_spectacular.utils import extend_schema
 
 
@@ -26,7 +20,7 @@ def get_or_none(classmodel, function, **kwargs):
 class IastVulRecheckPayloadSerializer(serializers.Serializer):
     value = serializers.CharField()
     status = serializers.IntegerField()
-    strategy_id = serializers.IntegerField()
+    strategy_id = serializers.IntegerField(min_value=1)
     language_id = serializers.IntegerField()
 
 
