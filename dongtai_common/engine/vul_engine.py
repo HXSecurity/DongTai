182bba3c55ae80af8acc7846333e9dd7e49f7135
bidaya0 <xianzelin@huoxian.cn>
2023-07-28 16:00:41 +0800

    feat: network scan detect multi path.

--- dongtai_common/engine/vul_engine.py
+++ dongtai_common/engine/vul_engine.py
@@ -200,16 +200,17 @@ class VulEngine:
         final_path = []
         for path in total_path_list:
             find_index = None
+            # Merge if path take same node
             for ind, target_path in enumerate(final_path):
-                if path[-1] == target_path[-1] or (set(path[1:]) & set(target_path[1:])):
+                if set(path[1:]) & set(target_path[1:]):
                     find_index = ind
                     break
             if find_index is not None:
                 target_path = final_path[find_index]
+                # Merge, except for sink nodes
                 final_path[find_index] = sorted(set(target_path + path[:-1]))
             else:
                 final_path.append(path)
-        final_stack = []
         for total_path in final_path:
             final_stack = []
             s = total_path[0]
