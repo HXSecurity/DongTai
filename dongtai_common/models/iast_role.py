99f2bbbc0f2aadfd6dacf060d1b33209f9f7a0c9
st1020 <gaoyishi@huoxian.cn>
2023-08-03 12:13:21 +0800

    feat: add unique constraint to name field

--- dongtai_common/models/iast_role.py
+++ dongtai_common/models/iast_role.py
@@ -19,3 +19,4 @@ class IastRoleV2(models.Model):
     class Meta:
         managed = get_managed()
         db_table = "iast_role"
+        indexes = [models.Index(fields=["name"])]
--- dongtai_common/models/project_group.py
+++ dongtai_common/models/project_group.py
@@ -17,6 +17,7 @@ class IastProjectGroup(models.Model):
     class Meta:
         managed = get_managed()
         db_table = "iast_project_group"
+        indexes = [models.Index(fields=["name"])]
 
 
 class IastProjectGroupProject(models.Model):
