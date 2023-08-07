0f196e86bac84e6ec31359f649f1d6082a484869
st1020 <gaoyishi@huoxian.cn>
2023-08-07 11:41:02 +0800

    refactor: vul engine

--- dongtai_common/migrations/0008_alter_iastprojectmetadata_project.py
+++ dongtai_common/migrations/0008_alter_iastprojectmetadata_project.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("dongtai_common", "0006_auto_20230803_1407"),
+        ("dongtai_common", "0007_auto_20230803_1810"),
     ]
 
     operations = [
