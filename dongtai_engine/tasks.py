0df5f77f4eb765a9b5de2236120a1db233917718
bidaya0 <xianzelin@huoxian.cn>
2023-08-07 17:33:11 +0800

    fix: account project auth .

--- dongtai_engine/tasks.py
+++ dongtai_engine/tasks.py
@@ -111,7 +111,7 @@ def search_and_save_vul(
         )
         project_time_stamp_update.apply_async((method_pool_model.agent.bind_project_id,), countdown=5)
         project_version_time_stamp_update.apply_async((method_pool_model.agent.project_version_id,), countdown=5)
-    validator_nodes = list(filter(lambda x: x["policyType"] == "validator", method_pool))
+    validator_nodes = list(filter(lambda x: "policyType" in x and x["policyType"] == "validator", method_pool))
     for validator_node in validator_nodes:
         validator_node["taint_input"] = validator_node["sourceValues"]
         validator_node["validator_method"] = f"{validator_node['className']}.{validator_node['methodName']}"
