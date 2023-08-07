516e1b76d3b39f0f3ba32f50fb062783238dc921
bidaya0 <xianzelin@huoxian.cn>
2023-08-07 16:47:34 +0800

    feat: add validated in vul stack check.

--- test/apiserver/test_agent_normal_vuln_validated.py
+++ test/apiserver/test_agent_normal_vuln_validated.py
@@ -1,5 +1,6 @@
 import json
 from test.apiserver.test_agent_base import AgentTestCase
+from dongtai_common.models.vulnerablity import IastVulnerabilityModel
 
 with open("./test/integration/mockdata/validated_method_pool.json") as fp:
     json_1 = json.load(fp)
@@ -9,3 +10,6 @@ class AgentNormalMultiVulTestCase(AgentTestCase):
     def test_agent_validated_vuln_upload(self):
         json_1["detail"]["agentId"] = self.agent_id
         self.agent_report(json_1)
+        vul = IastVulnerabilityModel.objects.filter(agent_id=self.agent_id, level_id=1).first()
+        self.assertIsNotNone(vul)
+        self.assertEqual(len(list(filter(lambda x: "validators" in x, json.loads(vul.full_stack)[0]))), 1)
