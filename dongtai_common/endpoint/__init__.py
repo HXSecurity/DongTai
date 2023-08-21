c0791da50df5983a0454c929304def82cbf4c595
st1020 <gaoyishi@huoxian.cn>
2023-08-21 18:34:14 +0800

    feat: remove outdate code

--- dongtai_common/endpoint/__init__.py
+++ dongtai_common/endpoint/__init__.py
@@ -96,23 +96,6 @@ class EndPoint(APIView):
         self.request = request
         self.headers = self.default_response_headers  # deprecate?
 
-        is_protocol_api = False
-        try:
-            if self.request.method is not None:
-                _path, _path_regex, _schema, filepath = VIEW_CLASS_TO_SCHEMA[self.__class__][self.request.method]
-                is_protocol_api = "dongtai_protocol" in filepath
-        except Exception:
-            pass
-
-        if not is_protocol_api and not request.user.is_active and not request.user.is_anonymous:
-            logout(request)
-            request.session.delete()
-            response = R.failure(msg="用户已经禁用", status_code=403)
-            request.session.delete()
-            response.delete_cookie(key=settings.CSRF_COOKIE_NAME, domain=settings.SESSION_COOKIE_DOMAIN)
-            response.delete_cookie(key="sessionid", domain=settings.SESSION_COOKIE_DOMAIN)
-            return response
-
         try:
             self.initial(request, *args, **kwargs)
 
